89f6131848736241685b9d3f832223efdd3a5410
akchu <akchu@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2004-10-03 17:20:48 +0000

    1) Created MSTP_Create_And_Send_Frame function. 2) Moved MSTP_Create_Frame call out of rs485.c and into mstp.c 3) Modified all previous calls to RS485_Send_Frame calls to call MSTP_Create_And_Send_Frame instead.

--- trunk/bacnet-stack/mstp.h
+++ trunk/bacnet-stack/mstp.h
@@ -231,13 +231,4 @@ void MSTP_Millisecond_Timer(struct mstp_port_struct_t *mstp_port);
 void MSTP_Receive_Frame_FSM(struct mstp_port_struct_t *mstp_port);
 void MSTP_Master_Node_FSM(struct mstp_port_struct_t *mstp_port);
 
-unsigned MSTP_Create_Frame(
-  uint8_t *buffer, // where frame is loaded
-  unsigned buffer_len, // amount of space available
-  uint8_t frame_type, // type of frame to send - see defines
-  uint8_t destination, // destination address
-  uint8_t source,  // source address
-  uint8_t *data, // any data to be sent - may be null
-  unsigned data_len); // number of bytes of data (up to 501)
-
 #endif
