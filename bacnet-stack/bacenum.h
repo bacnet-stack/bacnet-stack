5b4c6bbf74d51c0268616bea5fdb14fdec9b76ab
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2005-03-09 21:20:40 +0000

    added pdu type enumerations

--- bacnet-stack/bacenum.h
+++ bacnet-stack/bacenum.h
@@ -624,6 +624,19 @@ typedef enum {
     BACNET_APPLICATION_TAG_RESERVED3 = 15,
 } BACNET_APPLICATION_TAG;
 
+// note: these are not the real values,
+// but are shifted left for easy encoding
+typedef enum {
+    PDU_TYPE_CONFIRMED_SERVICE_REQUEST = 0,
+    PDU_TYPE_UNCONFIRMED_SERVICE_REQUEST = 0x10,
+    PDU_TYPE_SIMPLE_ACK = 0x20,
+    PDU_TYPE_COMPLEX_ACK = 0x30,
+    PDU_TYPE_SEGMENT_ACK = 0x40,
+    PDU_TYPE_ERROR = 0x50,
+    PDU_TYPE_REJECT = 0x60,
+    PDU_TYPE_ABORT = 0x70
+} BACNET_PDU_TYPE;
+
 typedef enum
 {
    // Alarm and Event Services
