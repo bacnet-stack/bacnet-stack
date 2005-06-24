f081bf3cf2a0f03d5742fb59a2434a9d9e24dd3c
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2005-06-24 12:41:42 +0000

    created while adding ReadPropertyMultiple service

---
+++ bacnet-stack/rpm.h
@@ -0,0 +1,99 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2005 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef RPM_H
+#define RPM_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct BACnet_Property_Reference
+{
+  BACNET_PROPERTY_ID object_property;
+  int32_t array_index;
+  uint8_t *application_data;
+  int application_data_len;
+} BACNET_PROPERTY_REFERENCE;
+
+typedef struct BACnet_Read_Property_Multiple_Data
+{
+  BACNET_OBJECT_TYPE object_type;
+  uint32_t object_instance;
+  BACNET_PROPERTY_REFERENCE *property_list;
+  unsigned property_list_len;
+} BACNET_READ_PROPERTY_MULTIPLE_DATA;
+
+// encode service
+int rpm_encode_apdu(
+  uint8_t *apdu, 
+  uint8_t invoke_id,
+  BACNET_READ_PROPERTY_MULTIPLE_DATA **data);
+
+// decode the service request only
+int rpm_decode_service_request(
+  uint8_t *apdu,
+  unsigned apdu_len,
+  BACNET_READ_PROPERTY_MULTIPLE_DATA **data);
+  
+int rpm_decode_apdu(
+  uint8_t *apdu,
+  unsigned apdu_len,
+  uint8_t *invoke_id,
+  BACNET_READ_PROPERTY_MULTIPLE_DATA **data);
+
+int rpm_ack_encode_apdu(
+  uint8_t *apdu,
+  uint8_t invoke_id,
+  BACNET_READ_PROPERTY_MULTIPLE_DATA **data);
+
+int rpm_ack_decode_service_request(
+  uint8_t *apdu,
+  int apdu_len, // total length of the apdu
+  BACNET_READ_PROPERTY_MULTIPLE_DATA **data);
+
+int rpm_ack_decode_apdu(
+  uint8_t *apdu,
+  int apdu_len, // total length of the apdu
+  uint8_t *invoke_id,
+  BACNET_READ_PROPERTY_MULTIPLE_DATA **data);
+
+  
+#ifdef TEST
+#include "ctest.h"
+
+void test_ReadProperty(Test * pTest);
+void test_ReadPropertyAck(Test * pTest);
+#endif
+
+#endif
+
