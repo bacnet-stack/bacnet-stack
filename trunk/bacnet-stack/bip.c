2f148ef14a76639f679cdf43727b4638197e391b
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2005-12-04 00:25:10 +0000

    changed type to remove warning with gcc

--- trunk/bacnet-stack/bip.c
+++ trunk/bacnet-stack/bip.c
@@ -217,7 +217,7 @@ uint16_t bip_receive(
     int max;
     struct timeval select_timeout;
     struct sockaddr_in sin = {-1};
-    int sin_len = sizeof(sin);
+    socklen_t sin_len = sizeof(sin);
 
     /* Make sure the socket is open */
     if (BIP_Socket < 0)
