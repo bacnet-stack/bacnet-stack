24fb3a4751461f643fd6bb6171a86a8f5dcb6c6a
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2006-02-08 17:09:47 +0000

    removed duplicate function

--- trunk/bacnet-stack/ports/win32/bip-init.c
+++ trunk/bacnet-stack/ports/win32/bip-init.c
@@ -43,11 +43,6 @@
 #include "bip.h"
 #include "net.h"
 
-void bip_set_interface(char *ifname)
-{
-  /*dummy function - to make the demos compile easier */
-}
-
 /* To fill a need, we invent the gethostaddr() function. */
 static long gethostaddr(void)
 {
