b0a7f7b557468a50b83f956c436e7dd8a5014eae
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2006-08-29 20:57:30 +0000

    Updated files for PIC18F port.

--- trunk/bacnet-stack/ports/pic18/dlmstp.c
+++ trunk/bacnet-stack/ports/pic18/dlmstp.c
@@ -26,6 +26,9 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#if PRINT_ENABLED
+#include <stdio.h>
+#endif
 #include "bacdef.h"
 #include "mstp.h"
 #include "dlmstp.h"
@@ -60,9 +63,8 @@ int dlmstp_send_pdu(BACNET_ADDRESS * dest,      /* destination address */
     uint8_t * pdu,              /* any data to be sent - may be null */
     unsigned pdu_len)
 {                               /* number of bytes of data */
-    bool status;
     int bytes_sent = 0;
-    int npdu_len = 0;
+    unsigned npdu_len = 0;
     uint8_t frame_type = 0;
     uint8_t destination = 0;    /* destination address */
     BACNET_ADDRESS src;
@@ -92,7 +94,8 @@ int dlmstp_send_pdu(BACNET_ADDRESS * dest,      /* destination address */
             return -4;
         }
         memmove(&PDU_Buffer[npdu_len], pdu, pdu_len);
-        bytes_sent = MSTP_Create_Frame(&MSTP_Port.TxBuffer[0],
+        bytes_sent = MSTP_Create_Frame(
+            (uint8_t *) & MSTP_Port.TxBuffer[0],
             sizeof(MSTP_Port.TxBuffer),
             MSTP_Port.TxFrameType,
             destination,
@@ -102,15 +105,6 @@ int dlmstp_send_pdu(BACNET_ADDRESS * dest,      /* destination address */
     }
 
     return bytes_sent;
-}
-
-void dlmstp_task(void)
-{
-    RS485_Check_UART_Data(&MSTP_Port);
-    MSTP_Receive_Frame_FSM(&MSTP_Port);
-
-    RS485_Process_Tx_Message();
-    MSTP_Master_Node_FSM(&MSTP_Port);
 }
 
 /* called about once a millisecond */
@@ -129,6 +123,16 @@ uint16_t dlmstp_receive(BACNET_ADDRESS * src,   /* source address */
     uint16_t pdu_len = 0;
 
     (void) timeout;
+    /* only do receive state machine while we don't have a frame */
+    if ((MSTP_Port.ReceivedValidFrame == false) &&
+        (MSTP_Port.ReceivedInvalidFrame == false)) {
+        RS485_Check_UART_Data(&MSTP_Port);
+        MSTP_Receive_Frame_FSM(&MSTP_Port);
+    }
+    /* only do master state machine while rx is idle */
+    if (MSTP_Port.receive_state == MSTP_RECEIVE_STATE_IDLE) {
+        while (MSTP_Master_Node_FSM(&MSTP_Port)) {};
+    }
     /* see if there is a packet available */
     if (Receive_Buffer.ready) {
         memmove(src, &Receive_Buffer.address,
@@ -141,14 +145,46 @@ uint16_t dlmstp_receive(BACNET_ADDRESS * src,   /* source address */
     return pdu_len;
 }
 
+void dlmstp_fill_bacnet_address(BACNET_ADDRESS * src, uint8_t mstp_address)
+{
+    int i = 0;
+
+    if (mstp_address == MSTP_BROADCAST_ADDRESS) {
+        /* mac_len = 0 if broadcast address */
+        src->mac_len = 0;
+        src->mac[0] = 0;
+    } else {
+        src->mac_len = 1;
+        src->mac[0] = mstp_address;
+    }
+    /* fill with 0's starting with index 1; index 0 filled above */
+    for (i = 1; i < MAX_MAC_LEN; i++) {
+        src->mac[i] = 0;
+    }
+    src->net = 0;
+    src->len = 0;
+    for (i = 0; i < MAX_MAC_LEN; i++) {
+        src->adr[i] = 0;
+    }
+}
+
 /* for the MS/TP state machine to use for putting received data */
-uint16_t dlmstp_put_receive(BACNET_ADDRESS * src,       /* source address */
+uint16_t dlmstp_put_receive(uint8_t src,        /* source MS/TP address */
     uint8_t * pdu,              /* PDU data */
     uint16_t pdu_len)
 {
-    memmove(&Receive_Buffer.address, src, sizeof(Receive_Buffer.address));
-    Receive_Buffer.pdu_len = pdu_len;
-    memmove(Receive_Buffer.pdu, pdu, sizeof(Receive_Buffer.pdu));
+    if (Receive_Buffer.ready) {
+        /* FIXME: what to do when we miss a message? */
+        pdu_len = 0;
+    } else if (pdu_len < sizeof(Receive_Buffer.pdu)) {
+        dlmstp_fill_bacnet_address(&Receive_Buffer.address, src);
+        Receive_Buffer.pdu_len = pdu_len;
+        memmove(Receive_Buffer.pdu, pdu, pdu_len);
+        Receive_Buffer.ready = true;
+    } else {
+        /* FIXME: message too large? */
+        pdu_len = 0;
+    }
 
     return pdu_len;
 }
