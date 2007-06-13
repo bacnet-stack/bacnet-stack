f4fc788e9ca657fef7ac2f177c0a4a25a01dc809
s3rock <s3rock@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-06-13 18:45:59 +0000

    Added #ifndef's to allow configuration parameters to be set in the build

--- trunk/bacnet-stack/config.h
+++ trunk/bacnet-stack/config.h
@@ -9,22 +9,26 @@
 /* This is used in constructing messages and to tell others our limits */
 /* 50 is the minimum; adjust to your memory and physical layer constraints */
 /* Lon=206, MS/TP=480, ARCNET=480, Ethernet=1476 */
-#define MAX_APDU 50
-/* #define MAX_APDU 480 */
-/* #define MAX_APDU 1476 */
-
+#ifndef MAX_APDU
+  #define MAX_APDU 50
+  /* #define MAX_APDU 480 */
+  /* #define MAX_APDU 1476 */
+#endif
 /* for confirmed messages, this is the number of transactions */
 /* that we hold in a queue waiting for timeout. */
 /* Configure to zero if you don't want any confirmed messages */
 /* Configure from 1..255 for number of outstanding confirmed */
 /* requests available. */
-#define MAX_TSM_TRANSACTIONS 255
-
+#ifndef MAX_TSM_TRANSACTIONS
+  #define MAX_TSM_TRANSACTIONS 255
+#endif
 /* The address cache is used for binding to BACnet devices */
 /* The number of entries corresponds to the number of */
 /* devices that might respond to an I-Am on the network. */
 /* If your device is a simple server and does not need to bind, */
 /* then you don't need to use this. */
-#define MAX_ADDRESS_CACHE 255
+#ifndef MAX_ADDRESS_CACHE
+  #define MAX_ADDRESS_CACHE 255
+#endif
 
 #endif
