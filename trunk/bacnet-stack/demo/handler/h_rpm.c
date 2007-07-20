2329d41655d28a3e25d9d33933c50aff80e6e91e
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-07-20 00:42:49 +0000

    Cleaned up device object and server demo for MS/TP properties and compiler warnings.

--- trunk/bacnet-stack/demo/handler/h_rpm.c
+++ trunk/bacnet-stack/demo/handler/h_rpm.c
@@ -1,7 +1,7 @@
 /**************************************************************************
 *
 * Copyright (C) 2007 Steve Karg <skarg@users.sourceforge.net>
-* Inspired by John Stachler.
+* Inspired by John Stachler <John.Stachler@lennoxind.com> 
 *
 * Permission is hereby granted, free of charge, to any person obtaining
 * a copy of this software and associated documentation files (the
