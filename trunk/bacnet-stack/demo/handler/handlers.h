ee5e11edbe2960273974ab4853cf7d6c6813e9b8
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-07-20 18:46:02 +0000

    Refactored ReadPropertyMultiple handler.

--- trunk/bacnet-stack/demo/handler/handlers.h
+++ trunk/bacnet-stack/demo/handler/handlers.h
@@ -107,6 +107,19 @@ extern "C" {
         BACNET_ADDRESS * src,
         BACNET_CONFIRMED_SERVICE_DATA * service_data);
 
+    /* Encodes the property APDU and returns the length,
+    or sets the error, and returns -1 */
+    /* resides in h_rp.c */
+    int Encode_Property_APDU(
+        uint8_t * apdu,
+        BACNET_OBJECT_TYPE object_type,
+        uint32_t object_instance,
+        BACNET_PROPERTY_ID property,
+        int32_t array_index,
+        BACNET_ERROR_CLASS * error_class,
+        BACNET_ERROR_CODE * error_code);
+
+
 
 #ifdef __cplusplus
 }
