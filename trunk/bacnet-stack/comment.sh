e66267c7b5bd69309da26d2509cb418cbca43d14
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-07-25 21:01:24 +0000

    Updated the script to check if ccmtcnvt exists.

--- trunk/bacnet-stack/comment.sh
+++ trunk/bacnet-stack/comment.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
+# This script converts any C++ comments to C comments
+# using the ccmtcnvt tool from the liwc package
+
+# silent fail if the tool is not installed
+[ -x /usr/bin/ccmtcnvt ] || exit 0
+
 directory=${1-`pwd`}
 for filename in $( find $directory -name '*.c' )
 do
   echo Converting $filename
-  ccmtcnvt $filename > /tmp/ccmtcnvt.karg
+  /usr/bin/ccmtcnvt $filename > /tmp/ccmtcnvt.karg
   mv /tmp/ccmtcnvt.karg $filename
 done
 
 for filename in $( find $directory -name '*.h' )
 do
   echo Converting $filename
-  ccmtcnvt $filename > /tmp/ccmtcnvt.karg
+  /usr/bin/ccmtcnvt $filename > /tmp/ccmtcnvt.karg
   mv /tmp/ccmtcnvt.karg $filename
 done
 
