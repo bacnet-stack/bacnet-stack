f14968abeccda5fc2554e7633e3ea53a3806528f
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-08-08 23:33:30 +0000

    Added port for Atmel AT91SAM7S-EK ARM7 board using the YAGARTO GNU ARM tool suite.  Still testing.

---
+++ trunk/bacnet-stack/ports/at91sam7s/main.c
@@ -0,0 +1,183 @@
+/**************************************************************************
+*
+* Copyright (C) 2007 Steve Karg <skarg@users.sourceforge.net>
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*********************************************************************/
+#include "AT91SAM7S256.h"
+#include "board.h"
+#include "math.h"
+#include "stdlib.h"
+#include "string.h"
+#include "stdbool.h"
+/* BACnet */
+#include "rs485.h"
+#include "datalink.h"
+#include "npdu.h"
+#include "apdu.h"
+#include "dcc.h"
+#include "iam.h"
+#include "handlers.h"
+
+//  *******************************************************
+//   FIXME: put in header files         External References
+//  *******************************************************
+extern	void LowLevelInit(void);
+extern	unsigned enableIRQ(void);
+extern	unsigned enableFIQ(void);
+
+extern	void TimerInit(void);
+extern volatile unsigned long Timer_Milliseconds;
+
+//  *******************************************************
+//               Global Variables - ???
+//  *******************************************************
+unsigned int FiqCount = 0;
+
+
+static unsigned long LED_Timer_1 = 0;
+static unsigned long LED_Timer_2 = 0;
+static unsigned long LED_Timer_3 = 0;
+static unsigned long LED_Timer_4 = 1000;
+
+void millisecond_timer(void)
+{
+    while (Timer_Milliseconds) {
+        Timer_Milliseconds--;
+        if (LED_Timer_1) {
+            LED_Timer_1--;
+        }
+        if (LED_Timer_2) {
+            LED_Timer_2--;
+        }
+        if (LED_Timer_3) {
+            LED_Timer_3--;
+        }
+        if (LED_Timer_4) {
+            LED_Timer_4--;
+        }
+        dlmstp_millisecond_timer();
+    }
+}
+
+int	main (void) {
+    unsigned long	IdleCount = 0;						// idle loop blink counter (2x)
+    bool LED3_Off_Enabled = true;
+    uint16_t pdu_len = 0;
+    BACNET_ADDRESS src; /* source address */
+    uint8_t pdu[MAX_MPDU]; /* PDU data */
+    
+    // Initialize the Atmel AT91SAM7S256 (watchdog, PLL clock, default interrupts, etc.)
+    LowLevelInit();
+    TimerInit();
+    /* Initialize the Parallel I/O Controller A Peripheral Clock */
+    volatile AT91PS_PMC	pPMC = AT91C_BASE_PMC; 
+    pPMC->PMC_PCER = pPMC->PMC_PCSR | (1<<AT91C_ID_PIOA);
+
+    // Set up the LEDs (PA0 - PA3)
+    volatile AT91PS_PIO pPIO = AT91C_BASE_PIOA;			// pointer to PIO data structure
+    pPIO->PIO_PER = LED_MASK | SW1_MASK;				// PIO Enable Register - allow PIO to control pins P0 - P3 and pin 19
+    pPIO->PIO_OER = LED_MASK;							// PIO Output Enable Register - sets pins P0 - P3 to outputs
+    pPIO->PIO_SODR = LED_MASK;							// PIO Set Output Data Register - turns off the four LEDs
+    
+    // Select PA19 (pushbutton) to be FIQ function (Peripheral B)
+    pPIO->PIO_BSR = SW1_MASK;
+    
+    // Set up the AIC registers for FIQ (pushbutton SW1)
+    volatile AT91PS_AIC pAIC = AT91C_BASE_AIC;			// pointer to AIC data structure
+    pAIC->AIC_IDCR = (1<<AT91C_ID_FIQ);					// Disable FIQ interrupt in AIC Interrupt Disable Command Register			
+    pAIC->AIC_SMR[AT91C_ID_FIQ] =						// Set the interrupt source type in AIC Source 
+        (AT91C_AIC_SRCTYPE_INT_EDGE_TRIGGERED);  		// Mode Register[0]
+    pAIC->AIC_ICCR = (1<<AT91C_ID_FIQ); 				// Clear the FIQ interrupt in AIC Interrupt Clear Command Register
+    pAIC->AIC_IDCR = (0<<AT91C_ID_FIQ);					// Remove disable FIQ interrupt in AIC Interrupt Disable Command Register			
+    pAIC->AIC_IECR = (1<<AT91C_ID_FIQ); 				// Enable the FIQ interrupt in AIC Interrupt Enable Command Register
+    
+#if defined(BACDL_MSTP)
+    RS485_Set_Baud_Rate(38400);
+    dlmstp_set_mac_address(55);
+    dlmstp_set_max_master(127);
+    dlmstp_set_max_info_frames(1);
+    dlmstp_init(NULL);
+#endif
+
+#ifndef DLMSTP_TEST
+    /* we need to handle who-is to support dynamic device binding */
+    apdu_set_unconfirmed_handler(SERVICE_UNCONFIRMED_WHO_IS,
+        handler_who_is);
+    /* Set the handlers for any confirmed services that we support. */
+    /* We must implement read property - it's required! */
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_READ_PROPERTY,
+        handler_read_property);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_REINITIALIZE_DEVICE,
+        handler_reinitialize_device);
+    apdu_set_confirmed_handler(SERVICE_CONFIRMED_WRITE_PROPERTY,
+        handler_write_property);
+    /* handle communication so we can shutup when asked */
+    apdu_set_confirmed_handler
+        (SERVICE_CONFIRMED_DEVICE_COMMUNICATION_CONTROL,
+        handler_device_communication_control);
+#endif
+
+    // enable interrupts
+    enableIRQ();
+    enableFIQ();
+
+    // endless blink loop
+    while (1) {
+        millisecond_timer();
+        /* interrupt turns on the LED, we turn it off */
+        if  (((pPIO->PIO_ODSR & LED3) == LED3) && (LED3_Off_Enabled))
+        {
+            LED3_Off_Enabled = false;
+            /* wait */
+            LED_Timer_3 = 250; 
+        } 
+        if (!LED_Timer_3) {
+            /* turn LED3 (DS3) off */
+            pPIO->PIO_SODR = LED3;
+            LED3_Off_Enabled = true;
+        }
+        if (!LED_Timer_4) {
+            if  ((pPIO->PIO_ODSR & LED4) == LED4)
+                pPIO->PIO_CODR = LED4;							// turn LED2 (DS2) on	
+            else
+                pPIO->PIO_SODR = LED4;							// turn LED2 (DS2) off
+            /* wait */
+            LED_Timer_4 = 1000;
+        }
+        
+        IdleCount++;									// count # of times through the idle loop
+                                            
+        /* BACnet handling */
+        pdu_len = datalink_receive(&src, &pdu[0], MAX_MPDU, 0);
+        if (pdu_len) {
+            pPIO->PIO_CODR = LED3;
+#ifndef DLMSTP_TEST
+            npdu_handler(&src, &pdu[0], pdu_len);
+#endif
+        }
+    }
+}
+
+
+
+
+
---
+++ trunk/bacnet-stack/ports/at91sam7s/rs485.c
@@ -0,0 +1,222 @@
+/**************************************************************************
+*
+* Copyright (C) 2007 Steve Karg <skarg@users.sourceforge.net>
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*********************************************************************/
+
+/* The module handles sending data out the RS-485 port */
+/* and handles receiving data from the RS-485 port. */
+/* Customize this file for your specific hardware */
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "mstp.h"
+
+/* This file has been customized for use with UART0 
+   on the AT91SAM7S-EK */
+#include "board.h"
+
+/* UART */
+static AT91S_USART *RS485_Interface = AT91C_BASE_US0;
+/* baud rate */
+static int RS485_Baud = 38400;
+
+/****************************************************************************
+* DESCRIPTION: Sets the interface - UART0 or UART1 or...
+* RETURN:      none
+* ALGORITHM:   none
+* NOTES:       none
+*****************************************************************************/
+void RS485_Set_Interface(char *ifname)
+{
+    RS485_Interface = (AT91S_USART *)ifname;
+}
+
+/****************************************************************************
+* DESCRIPTION: Initializes the RS485 hardware and variables, and starts in
+*              receive mode.
+* RETURN:      none
+* ALGORITHM:   none
+* NOTES:       none
+*****************************************************************************/
+void RS485_Initialize(void)
+{
+    // enable the USART0 peripheral clock
+    volatile AT91PS_PMC	pPMC = AT91C_BASE_PMC; 
+    pPMC->PMC_PCER = pPMC->PMC_PCSR | (1<<AT91C_ID_US0);
+
+    /* enable the peripheral by disabling the pin in the PIO controller */
+    *AT91C_PIOA_PDR = AT91C_PA5_RXD0 | AT91C_PA6_TXD0 | AT91C_PA7_RTS0;
+
+    RS485_Interface->US_CR = 
+        AT91C_US_RSTRX |          /* Reset Receiver      */
+        AT91C_US_RSTTX |          /* Reset Transmitter   */
+        AT91C_US_RXDIS |          /* Receiver Disable    */
+        AT91C_US_TXDIS;           /* Transmitter Disable */
+
+    RS485_Interface->US_MR = 
+        AT91C_US_USMODE_RS485  |  /* RS-485 Mode - RTS auto asserted */
+        AT91C_US_CLKS_CLOCK    |  /* Clock = MCK */
+        AT91C_US_CHRL_8_BITS   |  /* 8-bit Data  */
+        AT91C_US_PAR_NONE      |  /* No Parity   */
+        AT91C_US_NBSTOP_1_BIT;    /* 1 Stop Bit  */
+    
+    /* set the Time Guard to release RTS after x bit times */
+    RS485_Interface->US_TTGR = 4;
+
+    /* baud rate */
+    RS485_Interface->US_BRGR = MCK/16/RS485_Baud;
+
+    RS485_Interface->US_CR = 
+        AT91C_US_RXEN  |          /* Receiver Enable     */
+        AT91C_US_TXEN;            /* Transmitter Enable  */
+
+    return;
+}
+
+void RS485_Cleanup(void)
+{
+
+}
+
+/****************************************************************************
+* DESCRIPTION: Returns the baud rate that we are currently running at
+* RETURN:      none
+* ALGORITHM:   none
+* NOTES:       none
+*****************************************************************************/
+uint32_t RS485_Get_Baud_Rate(void)
+{
+    return RS485_Baud;
+}
+
+/****************************************************************************
+* DESCRIPTION: Sets the baud rate for the chip USART
+* RETURN:      none
+* ALGORITHM:   none
+* NOTES:       none
+*****************************************************************************/
+bool RS485_Set_Baud_Rate(uint32_t baud)
+{
+    bool valid = true;
+
+    switch (baud) {
+        case 9600:
+        case 19200:
+        case 38400:
+        case 57600:
+        case 115200:
+            RS485_Baud = baud;
+            RS485_Interface->US_BRGR = MCK/16/baud;
+            /* FIXME: store the baud rate */
+            break;
+        default:
+            valid = false;
+            break;
+    }
+
+    return valid;
+}
+
+/* Transmits a Frame on the wire */
+void RS485_Send_Frame(
+    volatile struct mstp_port_struct_t *mstp_port, /* port specific data */
+    uint8_t * buffer,  /* frame to send (up to 501 bytes of data) */
+    uint16_t nbytes)   /* number of bytes of data (up to 501) */
+{
+    uint8_t turnaround_time;
+    uint32_t baud;
+
+    /* toggle LED on send */
+    volatile AT91PS_PIO pPIO = AT91C_BASE_PIOA;
+    if  ((pPIO->PIO_ODSR & LED1) == LED1)
+        pPIO->PIO_CODR = LED1;
+    else
+        pPIO->PIO_SODR = LED1;
+    /* delay after reception - per MS/TP spec */
+    if (mstp_port) {
+        baud = RS485_Get_Baud_Rate();
+        /* wait about 40 bit times since reception */
+        if (baud == 9600)
+            turnaround_time = 4;
+        else if (baud == 19200)
+            turnaround_time = 2;
+        else
+            turnaround_time = 1;
+        while (mstp_port->SilenceTimer < turnaround_time) {
+            /* do nothing - wait for timer to increment */
+        };
+    }
+    while (nbytes) {
+        while (!(RS485_Interface->US_CSR & AT91C_US_TXRDY)) {
+            /* do nothing - wait for Tx buffer to get empty */
+        }        
+        RS485_Interface->US_THR = *buffer;
+        buffer++;
+        nbytes--;
+        /* per MSTP spec */
+        if (mstp_port) {
+            mstp_port->SilenceTimer = 0;
+        }
+    }
+
+    return;
+}
+
+/* called by timer, interrupt(?) or other thread */
+void RS485_Check_UART_Data(struct mstp_port_struct_t *mstp_port)
+{
+    if (mstp_port->ReceiveError == true) {
+        /* wait for state machine to clear this */
+    }
+    /* wait for state machine to read from the DataRegister */
+    else if (mstp_port->DataAvailable == false) {
+        /* check for data */
+        if ( RS485_Interface->US_CSR & AT91C_US_RXRDY) {
+            mstp_port->DataRegister = RS485_Interface->US_RHR;
+            mstp_port->DataAvailable = true;
+        }
+    }
+}
+
+#ifdef TEST_RS485
+static struct mstp_port_struct_t MSTP_Port;
+
+int main(void)
+{
+    unsigned i = 0;
+
+    RS485_Set_Baud_Rate(38400);
+    RS485_Initialize();
+    /* receive task */
+    for (;;) {
+        RS485_Check_UART_Data(&MSTP_Port);
+        if (MSTP_Port.DataAvailable) {
+                fprintf(stderr,"%02X ",MSTP_Port.DataRegister);
+                MSTP_Port.DataAvailable = false;
+        }
+    }
+}
+#endif                          /* TEST_ABORT */
+
