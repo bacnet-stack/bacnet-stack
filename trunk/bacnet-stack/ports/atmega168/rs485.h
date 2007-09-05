355252c8f3ed08577673283205f180b14de7424c
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-09-05 23:13:15 +0000

    Rewrote MS/TP to minimize memory usage.  Compiles, but untested.

--- trunk/bacnet-stack/ports/atmega168/rs485.h
+++ trunk/bacnet-stack/ports/atmega168/rs485.h
@@ -1,33 +1,42 @@
-/**************************************************************************
-*
-* Copyright (C) 2007 Steve Karg <skarg@users.sourceforge.net>
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-*********************************************************************/
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2004 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307
+ USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
 
 #ifndef RS485_H
 #define RS485_H
 
 #include <stdint.h>
-#include "mstp.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -35,14 +44,17 @@ extern "C" {
 
     void RS485_Initialize(void);
 
-    void RS485_Send_Frame(
-        volatile struct mstp_port_struct_t *mstp_port, /* port specific data */
-        uint8_t * buffer,  /* frame to send (up to 501 bytes of data) */
-        uint16_t nbytes);  /* number of bytes of data (up to 501) */
+    void RS485_Transmitter_Enable(bool enable);
 
-    uint8_t RS485_Check_UART_Data(
-        volatile struct mstp_port_struct_t *mstp_port); /* port specific data */
+    /* NOTE: Only able to send up to 255 bytes at a time */
+    void RS485_Send_Data(
+        uint8_t * buffer,       /* data to send */
+        uint8_t nbytes);       /* number of bytes of data */
 
+    bool RS485_ReceiveError(void);
+    bool RS485_DataAvailable(uint8_t *data);
+
+    void RS485_Turnaround_Delay(void);
     uint32_t RS485_Get_Baud_Rate(void);
     bool RS485_Set_Baud_Rate(uint32_t baud);
 
