83a821d75e7bbeba2885e12e1072b047e720dcf7
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-09-29 14:24:28 +0000

    Corrected poll station for sole master PFM transition.

--- trunk/bacnet-stack/ports/rtos32/mstp.c
+++ trunk/bacnet-stack/ports/rtos32/mstp.c
@@ -965,7 +965,7 @@ bool MSTP_Master_Node_FSM(volatile struct mstp_port_struct_t * mstp_port)
         } else if (next_poll_station == mstp_port->Next_Station) {
             if (mstp_port->SoleMaster == true) {
                 /* SoleMasterRestartMaintenancePFM */
-                mstp_port->Poll_Station = next_poll_station;
+                mstp_port->Poll_Station = next_next_station;
                 MSTP_Create_And_Send_Frame(mstp_port,
                     FRAME_TYPE_POLL_FOR_MASTER, mstp_port->Poll_Station,
                     mstp_port->This_Station, NULL, 0);
