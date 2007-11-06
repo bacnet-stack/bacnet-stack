26c5f8b6829ffab67af675c291a1d5c8b4750cad
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-11-06 17:55:22 +0000

    Added comment.

--- trunk/bacnet-stack/include/config.h
+++ trunk/bacnet-stack/include/config.h
@@ -36,7 +36,7 @@
 /* Typical sizes are 50, 128, 206, 480, 1024, and 1476 octets */
 /* This is used in constructing messages and to tell others our limits */
 /* 50 is the minimum; adjust to your memory and physical layer constraints */
-/* Lon=206, MS/TP=480, ARCNET=480, Ethernet=1476 */
+/* Lon=206, MS/TP=480, ARCNET=480, Ethernet=1476, BACnet/IP=64K */
 #if !defined(MAX_APDU)
     /* #define MAX_APDU 50 */
     #define MAX_APDU 480
