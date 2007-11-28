97b19c07c8b17b6c9205577b9a9ef09a87e93037
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2007-11-28 23:02:32 +0000

    running splint on the code.

--- bacnet-stack/splint.sh
+++ bacnet-stack/splint.sh
@@ -3,11 +3,17 @@
 [ -x /usr/bin/splint ] || exit 0
 
 INCLUDES="-Iinclude -Idemo/handler -Idemo/object -Iports/linux"
+SETTINGS="-castfcnptr -fullinitblock -weak +posixlib"
+
+if [ ! -x .splintrc ]
+then
+  echo ${INCLUDES} ${SETTINGS} > .splintrc
+fi
 
 directory=${1-`pwd`}
 for filename in $( find $directory -name '*.c' )
 do
   echo splinting ${filename}
-  /usr/bin/splint ${INCLUDES} ${filename}
+  /usr/bin/splint ${filename}
 done
 
