800993aecc099b79c24063da11307015c0af0cd6
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-04-25 19:35:04 +0000

    Updated files to be able to build under mingw32 for linux.

--- bacnet-stack/build.bat
+++ bacnet-stack/build.bat
@@ -7,3 +7,5 @@ rem %BORLAND_DIR%\bin\make -f makefile.b32 all
 rem Build for MinGW
 make BACNET_PORT=win32 OPTIMIZATION=-Os DEBUGGING= clean all
 
+rem On Linux, install mingw32 and use this:
+rem make BACNET_PORT=win32 OPTIMIZATION=-Os DEBUGGING= CC=i586-mingw32msvc-gcc AR=i586-mingw32msvc-ar clean all 
