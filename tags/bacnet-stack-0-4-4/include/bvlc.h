8a2527c7ad9990942b571aaa02ed6011d5d901f1
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-04-25 19:37:23 +0000

    Created version bacnet-stack-0.4.4

---
+++ tags/bacnet-stack-0-4-4/include/bvlc.h
@@ -0,0 +1,76 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2007 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef BVLC_H
+#define BVLC_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <time.h>
+#include "bacdef.h"
+#include "npdu.h"
+
+#ifdef __cplusplus
+extern "C" {
+
+#endif /* __cplusplus */
+
+#if defined(BBMD_ENABLED) && BBMD_ENABLED
+    void bvlc_maintenance_timer(
+        time_t seconds);
+#else
+#define bvlc_maintenance_timer(x)
+#endif
+    /* registers with a bbmd as a foreign device */
+    void bvlc_register_with_bbmd(
+        long bbmd_address,      /* in network byte order */
+        uint16_t bbmd_port,
+        uint16_t time_to_live_seconds);
+
+    uint16_t bvlc_receive(
+        BACNET_ADDRESS * src,   /* returns the source address */
+        uint8_t * npdu, /* returns the NPDU */
+        uint16_t max_npdu,      /* amount of space available in the NPDU  */
+        unsigned timeout);      /* number of milliseconds to wait for a packet */
+
+    int bvlc_send_pdu(
+        BACNET_ADDRESS * dest,  /* destination address */
+        BACNET_NPDU_DATA * npdu_data,   /* network information */
+        uint8_t * pdu,  /* any data to be sent - may be null */
+        unsigned pdu_len);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* */
---
+++ tags/bacnet-stack-0-4-4/include/rp.h
@@ -0,0 +1,109 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2005 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef READPROPERTY_H
+#define READPROPERTY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct BACnet_Read_Property_Data {
+    BACNET_OBJECT_TYPE object_type;
+    uint32_t object_instance;
+    BACNET_PROPERTY_ID object_property;
+    int32_t array_index;
+    uint8_t *application_data;
+    int application_data_len;
+} BACNET_READ_PROPERTY_DATA;
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* encode service */
+    int rp_encode_apdu(
+        uint8_t * apdu,
+        uint8_t invoke_id,
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+/* decode the service request only */
+    int rp_decode_service_request(
+        uint8_t * apdu,
+        unsigned apdu_len,
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+    /* method to encode the ack without extra buffer */
+    int rp_ack_encode_apdu_init(
+        uint8_t * apdu,
+        uint8_t invoke_id,
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+    int rp_ack_encode_apdu_object_property_end(
+        uint8_t * apdu);
+
+    /* method to encode the ack using extra buffer */
+    int rp_ack_encode_apdu(
+        uint8_t * apdu,
+        uint8_t invoke_id,
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+    int rp_ack_decode_service_request(
+        uint8_t * apdu,
+        int apdu_len,   /* total length of the apdu */
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+
+#ifdef TEST
+#include "ctest.h"
+    int rp_decode_apdu(
+        uint8_t * apdu,
+        unsigned apdu_len,
+        uint8_t * invoke_id,
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+    int rp_ack_decode_apdu(
+        uint8_t * apdu,
+        int apdu_len,   /* total length of the apdu */
+        uint8_t * invoke_id,
+        BACNET_READ_PROPERTY_DATA * rpdata);
+
+    void test_ReadProperty(
+        Test * pTest);
+    void test_ReadPropertyAck(
+        Test * pTest);
+#endif
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif
