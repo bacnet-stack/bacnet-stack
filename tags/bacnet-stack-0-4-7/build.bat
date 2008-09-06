453ad1b64fb86cd62461c990fa7a1a8a05e8391f
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-09-06 21:09:49 +0000

    Created version bacnet-stack-0.4.7

---
+++ tags/bacnet-stack-0-4-7/build.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Build with MinGW mingw.sourceforge.net
+make BACNET_PORT=win32 OPTIMIZATION=-Os DEBUGGING= clean all
+rem Build for MinGW debug
+rem make BACNET_PORT=win32 clean all
+
+rem On Linux, install mingw32 and use this:
+rem make BACNET_PORT=win32 OPTIMIZATION=-Os DEBUGGING= CC=i586-mingw32msvc-gcc AR=i586-mingw32msvc-ar clean all 
