2521ce6bae7fda31869eaca44e1fa835aebd0f57
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-10-21 22:44:39 +0000

    Testing with MS/TP.

--- trunk/bacnet-stack/build.bat
+++ trunk/bacnet-stack/build.bat
@@ -2,8 +2,12 @@
 echo Build with MinGW mingw.sourceforge.net
 echo Note: copy mingw32-make.exe to make.exe to build for Win32
 make BACNET_PORT=win32 OPTIMIZATION=-Os DEBUGGING= clean all
+
 rem Build for MinGW debug
 rem make BACNET_PORT=win32 clean all
+
+rem Build for MinGW MS/TP
+rem make BACNET_PORT=win32 DEBUGGING= BACDL_DEFINE=-DBACDL_MSTP=1 clean all
 
 rem On Linux, install mingw32 and use this:
 rem make BACNET_PORT=win32 OPTIMIZATION=-Os DEBUGGING= CC=i586-mingw32msvc-gcc AR=i586-mingw32msvc-ar clean all 
