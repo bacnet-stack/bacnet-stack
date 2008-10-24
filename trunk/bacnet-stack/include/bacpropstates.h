fd0ea6fe7d123bb0521e3a2f3645f21d231a244c
minack <minack@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-10-24 04:42:29 +0000

    Changes: Added missing copyright notices to new files.

--- trunk/bacnet-stack/include/bacpropstates.h
+++ trunk/bacnet-stack/include/bacpropstates.h
@@ -1,7 +1,6 @@
-
 /*####COPYRIGHTBEGIN####
  -------------------------------------------
- Copyright (C) 2006 Steve Karg
+ Copyright (C) 2008 John Minack
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
