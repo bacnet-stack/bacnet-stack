cb9ea9a2a2bb804759d538643850f9872ffe109a
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-11-20 16:26:39 +0000

    Added BUILD=debug to linux/mingw makefiles.

--- trunk/bacnet-stack/bin/bvlc.bat
+++ trunk/bacnet-stack/bin/bvlc.bat
@@ -1,9 +1,14 @@
-REM Example of parameters for Foreign Device Registration
-REM This CMD window will use port 47809 to communicate
+@echo off
+echo Example of parameters for Foreign Device Registration
+echo This CMD window will use port 47809 to communicate
+@echo on
 set BACNET_IP_PORT=47809
-REM The BBMD is located at the standard port 47808 and at
-REM the dotted IP address passed in on the command line.
-REM When the demo client applications see the BBMD address,
-REM they register as a Foreign Device to it.
+@echo off
+echo The BBMD is located at the standard port 47808 and at
+echo the dotted IP address passed in on the command line.
+echo When the demo client applications see the BBMD address,
+echo they register as a Foreign Device to it.
+@echo on
 set BACNET_BBMD_PORT=47808
 set BACNET_BBMD_ADDRESS=%1
+
