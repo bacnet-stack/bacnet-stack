fad256c8a0222f6fb723a4c0491c217751efcebf
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2008-11-26 14:02:41 +0000

    Corrected compiler warning.

--- trunk/bacnet-stack/demo/handler/h_rpm_a.c
+++ trunk/bacnet-stack/demo/handler/h_rpm_a.c
@@ -24,6 +24,7 @@
 *********************************************************************/
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "config.h"
 #include "config.h"
 #include "txbuf.h"
