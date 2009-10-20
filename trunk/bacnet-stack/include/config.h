63baa00fd66fb8f49769684cc86527e1bbe0d901
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2009-10-20 17:23:48 +0000

    Corrected BACnet/IP MAX APDU to have only a max of 1497 bytes.

--- trunk/bacnet-stack/include/config.h
+++ trunk/bacnet-stack/include/config.h
@@ -64,12 +64,12 @@
 /* Typical sizes are 50, 128, 206, 480, 1024, and 1476 octets */
 /* This is used in constructing messages and to tell others our limits */
 /* 50 is the minimum; adjust to your memory and physical layer constraints */
-/* Lon=206, MS/TP=480, ARCNET=480, Ethernet=1476, BACnet/IP=64K */
+/* Lon=206, MS/TP=480, ARCNET=480, Ethernet=1476, BACnet/IP=1476 */
 #if !defined(MAX_APDU)
     /* #define MAX_APDU 50 */
     /* #define MAX_APDU 1476 */
 #if defined(BACDL_BIP)
-#define MAX_APDU 16383
+#define MAX_APDU 1476
 #elif defined (BACDL_ETHERNET)
 #define MAX_APDU 1476
 #else
