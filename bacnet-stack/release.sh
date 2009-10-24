b6dd7f690c1824a9ac7f3ed2afd4eccf1aa9588d
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2009-10-24 18:44:41 +0000

    Changed expressions.

--- bacnet-stack/release.sh
+++ bacnet-stack/release.sh
@@ -19,68 +19,69 @@ fi
 DOTTED_VERSION="$1"
 DASHED_VERSION="$(echo "$1" | sed 's/[\.*]/-/g')"
 
-echo "Creating the release files for version $DOTTED_VERSION"
+echo "Creating the release files for version ${DOTTED_VERSION}"
 
-echo "Creating the $PROJECT change log..."
-rm $CHANGELOG
+echo "Creating the ${PROJECT} change log..."
+rm ${CHANGELOG}
 svn update
-svn log --xml --verbose | xsltproc svn2cl.xsl - > $CHANGELOG
-if [ -z "$CHANGELOG" ]
+svn log --xml --verbose | xsltproc svn2cl.xsl - > ${CHANGELOG}
+if [ -z "${CHANGELOG}" ]
 then
-echo "Failed to create $CHANGELOG"
+echo "Failed to create ${CHANGELOG}"
 else
-echo "$CHANGELOG created."
+echo "${CHANGELOG} created."
 fi
 
-ARCHIVE_NAME=$SVN_MODULE-$DOTTED_VERSION
+ARCHIVE_NAME=${SVN_MODULE}-${DOTTED_VERSION}
+TAGGED_NAME=${SVN_MODULE}-${DASHED_VERSION}
+SVN_BASE_URL=https://${PROJECT}.svn.sourceforge.net/svnroot/${PROJECT}
 
-SVN_TRUNK_NAME=https://$PROJECT.svn.sourceforge.net/svnroot/$PROJECT/trunk/$SVN_MODULE
-SVN_TAGGED_NAME=https://$PROJECT.svn.sourceforge.net/svnroot/$PROJECT/tags/$SVN_MODULE-$DASHED_VERSION
-echo "Setting a tag on the $SVN_MODULE module called $SVN_MODULE-$DASHED_VERSION"
-svn copy -m "Created version $ARCHIVE_NAME" $SVN_TRUNK_NAME $SVN_TAGGED_NAME
+SVN_TRUNK_NAME=${SVN_BASE_URL}/trunk/${SVN_MODULE}
+SVN_TAGGED_NAME=${SVN_BASE_URL}/tags/${TAGGED_NAME}
+echo "Setting a tag on the ${SVN_MODULE} module called ${TAGGED_NAME}"
+svn copy ${SVN_TRUNK_NAME} ${SVN_TAGGED_NAME} -m "Created version ${ARCHIVE_NAME}" 
 echo "done."
 
 echo "Getting a clean version out of subversion for Linux gzip"
-svn export $SVN_TAGGED_NAME $ARCHIVE_NAME
+svn export ${SVN_TAGGED_NAME} ${ARCHIVE_NAME}
 echo "done."
 
-GZIP_FILENAME=$ARCHIVE_NAME.tgz
+GZIP_FILENAME=${ARCHIVE_NAME}.tgz
 echo "tar and gzip the clean directory"
-tar -cvvzf $GZIP_FILENAME $ARCHIVE_NAME/
+tar -cvvzf ${GZIP_FILENAME} ${ARCHIVE_NAME}/
 echo "done."
 
-if [ -z "$GZIP_FILENAME" ]
+if [ -z "${GZIP_FILENAME}" ]
 then
-echo "Failed to create $GZIP_FILENAME"
+echo "Failed to create ${GZIP_FILENAME}"
 else
-echo "$GZIP_FILENAME created."
+echo "${GZIP_FILENAME} created."
 fi
 
-rm -rf $ARCHIVE_NAME
+rm -rf ${ARCHIVE_NAME}
 
 echo "Getting another clean version out of subversion for Windows zip"
-svn export --native-eol CRLF $SVN_TAGGED_NAME $ARCHIVE_NAME
-ZIP_FILENAME=$ARCHIVE_NAME.zip
+svn export --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME}
+ZIP_FILENAME=${ARCHIVE_NAME}.zip
 echo "done."
 echo "Zipping the directory exported for Windows."
-zip -r $ZIP_FILENAME $ARCHIVE_NAME
+zip -r ${ZIP_FILENAME} ${ARCHIVE_NAME}
 
-if [ -z "$ZIP_FILENAME" ]
+if [ -z "${ZIP_FILENAME}" ]
 then
-echo "Failed to create $ZIP_FILENAME"
+echo "Failed to create ${ZIP_FILENAME}"
 else
-echo "$ZIP_FILENAME created."
+echo "${ZIP_FILENAME} created."
 fi
 
-rm -rf $ARCHIVE_NAME
+rm -rf ${ARCHIVE_NAME}
 
 echo "Sending to SourceForge..."
 
-mkdir $ARCHIVE_NAME
-mv $ZIP_FILENAME $ARCHIVE_NAME
-mv $GZIP_FILENAME $ARCHIVE_NAME
-mv $CHANGELOG $ARCHIVE_NAME
-scp -r $ARCHIVE_NAME $FRS_URL
+mkdir ${ARCHIVE_NAME}
+mv ${ZIP_FILENAME} ${ARCHIVE_NAME}
+mv ${GZIP_FILENAME} ${ARCHIVE_NAME}
+mv ${CHANGELOG} ${ARCHIVE_NAME}
+#scp -r ${ARCHIVE_NAME} ${FRS_URL}
 
 echo "Complete!"
-
