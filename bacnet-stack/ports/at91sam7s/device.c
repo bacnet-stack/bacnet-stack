8d85b05afd81da9ec9d98af8e710e0346ae759bc
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2010-02-11 16:54:11 +0000

    Corrected name that caused compiler error.

--- bacnet-stack/ports/at91sam7s/device.c
+++ bacnet-stack/ports/at91sam7s/device.c
@@ -477,7 +477,7 @@ int Device_Encode_Property_APDU(
     return apdu_len;
 }
 
-bool Device_Write_Property_APDU(
+bool Device_Write_Property(
     BACNET_WRITE_PROPERTY_DATA * wp_data,
     BACNET_ERROR_CLASS * error_class,
     BACNET_ERROR_CODE * error_code)
