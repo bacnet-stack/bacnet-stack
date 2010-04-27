37d048310ccff1671b5a8cc2b4840ae33c8b289d
brayra <brayra@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2010-04-27 15:11:58 +0000

    Added ability to set starting invoke id from environment variable BACNET_INVOKE_ID

--- bacnet-stack/include/tsm.h
+++ bacnet-stack/include/tsm.h
@@ -106,6 +106,7 @@ extern "C" {
 /* use these in tandem */
     uint8_t tsm_next_free_invokeID(
         void);
+    void tsm_invokeID_set(uint8_t invokeID);
 /* returns the same invoke ID that was given */
     void tsm_set_confirmed_unsegmented_transaction(
         uint8_t invokeID,
