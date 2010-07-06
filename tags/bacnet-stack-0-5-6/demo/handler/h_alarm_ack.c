84b5591d4fba418d74f9c5b2844e1e4dd672d6c3
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2010-07-06 15:28:09 +0000

    Created version bacnet-stack-0.5.6

---
+++ tags/bacnet-stack-0-5-6/demo/handler/h_alarm_ack.c
@@ -0,0 +1,136 @@
+/**************************************************************************
+*
+* Copyright (C) 2005 Steve Karg <skarg@users.sourceforge.net>
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*********************************************************************/
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "config.h"
+#include "txbuf.h"
+#include "bacdef.h"
+#include "bacdcode.h"
+#include "apdu.h"
+#include "npdu.h"
+#include "abort.h"
+#include "alarm_ack.h"
+#include "handlers.h"
+
+/** @file h_alarm_ack.c  Handles Alarm Acknowledgment. */
+
+/** Handler for an Alarm/Event Acknowledgement.
+ * @ingroup ALMACK
+ * This handler will be invoked by apdu_handler() if it has been enabled
+ * by a call to apdu_set_confirmed_handler().
+ * This handler builds a response packet, which is
+ * - an Abort if
+ *   - the message is segmented
+ *   - if decoding fails
+ * - Otherwise, sends a simple ACK
+ * 
+ * @param service_request [in] The contents of the service request.
+ * @param service_len [in] The length of the service_request.
+ * @param src [in] BACNET_ADDRESS of the source of the message
+ * @param service_data [in] The BACNET_CONFIRMED_SERVICE_DATA information 
+ *                          decoded from the APDU header of this message. 
+ */
+void handler_alarm_ack(
+    uint8_t * service_request,
+    uint16_t service_len,
+    BACNET_ADDRESS * src,
+    BACNET_CONFIRMED_SERVICE_DATA * service_data)
+{
+    BACNET_ALARM_ACK_DATA data;
+    int len = 0;
+    int pdu_len = 0;
+    BACNET_NPDU_DATA npdu_data;
+    int bytes_sent = 0;
+    BACNET_ADDRESS my_address;
+
+    /* encode the NPDU portion of the packet */
+    datalink_get_my_address(&my_address);
+    npdu_encode_npdu_data(&npdu_data, false, MESSAGE_PRIORITY_NORMAL);
+    pdu_len =
+        npdu_encode_pdu(&Handler_Transmit_Buffer[0], src, &my_address,
+        &npdu_data);
+    if (service_data->segmented_message) {
+        /* we don't support segmentation - send an abort */
+        len =
+            abort_encode_apdu(&Handler_Transmit_Buffer[pdu_len],
+            service_data->invoke_id, ABORT_REASON_SEGMENTATION_NOT_SUPPORTED,
+            true);
+#if PRINT_ENABLED
+        fprintf(stderr, "Alarm Ack: Segmented message.  Sending Abort!\n");
+#endif
+        goto AA_ABORT;
+    }
+
+    len =
+        alarm_ack_decode_service_request(service_request, service_len, &data);
+#if PRINT_ENABLED
+    if (len <= 0)
+        fprintf(stderr, "Alarm Ack: Unable to decode Request!\n");
+#endif
+    if (len < 0) {
+        /* bad decoding - send an abort */
+        len =
+            abort_encode_apdu(&Handler_Transmit_Buffer[pdu_len],
+            service_data->invoke_id, ABORT_REASON_OTHER, true);
+#if PRINT_ENABLED
+        fprintf(stderr, "Alarm Ack: Bad Encoding.  Sending Abort!\n");
+#endif
+        goto AA_ABORT;
+    }
+
+    /*
+     ** Process Life Safety Operation Here
+     */
+#if PRINT_ENABLED
+    fprintf(stderr,
+        "Alarm Ack Operation: Received acknowledge for object id %lu from %s for process id %lu for object %lu\n",
+        (unsigned long) data.eventObjectIdentifier.instance,
+        data.ackSource.value, (unsigned long) data.ackProcessIdentifier,
+        (unsigned long) data.eventObjectIdentifier.instance);
+#endif
+
+    len =
+        encode_simple_ack(&Handler_Transmit_Buffer[pdu_len],
+        service_data->invoke_id, SERVICE_CONFIRMED_ACKNOWLEDGE_ALARM);
+#if PRINT_ENABLED
+    fprintf(stderr, "Alarm Acknowledge: " "Sending Simple Ack!\n");
+#endif
+
+  AA_ABORT:
+    pdu_len += len;
+    bytes_sent =
+        datalink_send_pdu(src, &npdu_data, &Handler_Transmit_Buffer[0],
+        pdu_len);
+#if PRINT_ENABLED
+    if (bytes_sent <= 0)
+        fprintf(stderr, "Alarm Acknowledge: " "Failed to send PDU (%s)!\n",
+            strerror(errno));
+#endif
+
+    return;
+}
---
+++ tags/bacnet-stack-0-5-6/demo/handler/h_whois.c
@@ -0,0 +1,110 @@
+/**************************************************************************
+*
+* Copyright (C) 2005 Steve Karg <skarg@users.sourceforge.net>
+*
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*
+*********************************************************************/
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include "config.h"
+#include "txbuf.h"
+#include "bacdef.h"
+#include "bacdcode.h"
+#include "whois.h"
+#include "iam.h"
+#include "device.h"
+#include "client.h"
+#include "txbuf.h"
+
+/** @file h_whois.c  Handles Who-Is requests. */
+
+/** Handler for Who-Is requests, with broadcast I-Am response.
+ * @ingroup DMDDB
+ * @param service_request [in] The received message to be handled.
+ * @param service_len [in] Length of the service_request message.
+ * @param src [in] The BACNET_ADDRESS of the message's source.
+ */
+void handler_who_is(
+    uint8_t * service_request,
+    uint16_t service_len,
+    BACNET_ADDRESS * src)
+{
+    int len = 0;
+    int32_t low_limit = 0;
+    int32_t high_limit = 0;
+
+    (void) src;
+    len =
+        whois_decode_service_request(service_request, service_len, &low_limit,
+        &high_limit);
+    if (len == 0)
+        Send_I_Am(&Handler_Transmit_Buffer[0]);
+    else if (len != -1) {
+        /* is my device id within the limits? */
+        if (((Device_Object_Instance_Number() >= (uint32_t) low_limit) &&
+                (Device_Object_Instance_Number() <= (uint32_t) high_limit))
+            ||
+            /* BACnet wildcard is the max instance number - everyone responds */
+            ((BACNET_MAX_INSTANCE >= (uint32_t) low_limit) &&
+                (BACNET_MAX_INSTANCE <= (uint32_t) high_limit)))
+            Send_I_Am(&Handler_Transmit_Buffer[0]);
+    }
+
+    return;
+}
+
+/** Handler for Who-Is requests, with Unicast I-Am response (per Addendum 135-2004q).
+ * @ingroup DMDDB
+ * @param service_request [in] The received message to be handled.
+ * @param service_len [in] Length of the service_request message.
+ * @param src [in] The BACNET_ADDRESS of the message's source.
+ */
+void handler_who_is_unicast(
+    uint8_t * service_request,
+    uint16_t service_len,
+    BACNET_ADDRESS * src)
+{
+    int len = 0;
+    int32_t low_limit = 0;
+    int32_t high_limit = 0;
+
+    (void) src;
+    len =
+        whois_decode_service_request(service_request, service_len, &low_limit,
+        &high_limit);
+    if (len == 0)
+        Send_I_Am_Unicast(&Handler_Transmit_Buffer[0], src);
+    else if (len != -1) {
+        /* is my device id within the limits? */
+        if (((Device_Object_Instance_Number() >= (uint32_t) low_limit) &&
+                (Device_Object_Instance_Number() <= (uint32_t) high_limit))
+            ||
+            /* BACnet wildcard is the max instance number - everyone responds */
+            ((BACNET_MAX_INSTANCE >= (uint32_t) low_limit) &&
+                (BACNET_MAX_INSTANCE <= (uint32_t) high_limit)))
+            Send_I_Am_Unicast(&Handler_Transmit_Buffer[0], src);
+    }
+
+    return;
+}
