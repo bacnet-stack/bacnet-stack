f26997a36797d3fcc9446f0b30c9e7c4d40b2f33
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2010-11-30 18:04:27 +0000

    Changed make to $(MAKE).  Not sure if this is still needed, since MSYS has make.exe (in lieu of MinGW).  Lots of demo makefiles still have make in them...

--- bacnet-stack/build.bat
+++ bacnet-stack/build.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Build with MinGW and MSYS: mingw.sourceforge.net
 rem set PATH=C:\MinGW\msys\1.0\bin;C:\MinGW\bin
+rem assumes rm, cp, size are already in path
 set CC=gcc
 set AR=ar
 set MAKE=make
-set SIZE=size
 make BACNET_PORT=win32 BUILD=release clean all
 
 rem Build for MinGW debug
