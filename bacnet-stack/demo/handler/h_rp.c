e48aab8ebb3f53bb6b42d87656b018ef6674ca42
petermcs <petermcs@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2011-03-14 11:11:09 +0000

    When reporting abort for apdu exceeding the maximum the requester can accomodate we need to set the error code up as read property processing may have overwritten the default by then.

--- bacnet-stack/demo/handler/h_rp.c
+++ bacnet-stack/demo/handler/h_rp.c
@@ -125,7 +125,10 @@ void handler_read_property(
             [npdu_len + apdu_len]);
         apdu_len += len;
         if (apdu_len > service_data->max_resp) {
-            /* too big for the sender - send an abort */
+            /* too big for the sender - send an abort
+             * Setting of error code needed here as read property processing may 
+             * have overriden the default set at start */
+			rpdata.error_code = ERROR_CODE_ABORT_SEGMENTATION_NOT_SUPPORTED;
             len = BACNET_STATUS_ABORT;
 #if PRINT_ENABLED
             fprintf(stderr, "RP: Message too large.\n");
