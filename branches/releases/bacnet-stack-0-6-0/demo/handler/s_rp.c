50d3d2b342987653e2b3dfc237da963fa6301eb2
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2011-08-31 17:04:29 +0000

    Merged changes from Trunk. Fixed or added comments. Fixed or added debug.

--- branches/releases/bacnet-stack-0-6-0/demo/handler/s_rp.c
+++ branches/releases/bacnet-stack-0-6-0/demo/handler/s_rp.c
@@ -55,7 +55,7 @@
  * @param array_index [in] Optional: if the Property is an array,
  *   - 0 for the array size
  *   - 1 to n for individual array members
- *   - -1 for the full array to be read.
+ *   - BACNET_ARRAY_ALL (~0) for the full array to be read.
  * @return invoke id of outgoing message, or 0 if device is not bound or no tsm available
  */
 uint8_t Send_Read_Property_Request(
