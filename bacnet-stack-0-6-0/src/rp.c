f7b1988c1d18cbc1a9ae337de58b67b93ac6dcb9
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2011-10-18 19:30:14 +0000

    Refactored.

--- bacnet-stack-0-6-0/src/rp.c
+++ bacnet-stack-0-6-0/src/rp.c
@@ -198,25 +198,9 @@ int rp_ack_encode_apdu(
     int apdu_len = 0;   /* total length of the apdu, return value */
 
     if (apdu) {
-        apdu[0] = PDU_TYPE_COMPLEX_ACK; /* complex ACK service */
-        apdu[1] = invoke_id;    /* original invoke id from request */
-        apdu[2] = SERVICE_CONFIRMED_READ_PROPERTY;      /* service choice */
-        apdu_len = 3;
-        /* service ack follows */
-        apdu_len +=
-            encode_context_object_id(&apdu[apdu_len], 0, rpdata->object_type,
-            rpdata->object_instance);
-        apdu_len +=
-            encode_context_enumerated(&apdu[apdu_len], 1,
-            rpdata->object_property);
-        /* context 2 array index is optional */
-        if (rpdata->array_index != BACNET_ARRAY_ALL) {
-            apdu_len +=
-                encode_context_unsigned(&apdu[apdu_len], 2,
-                rpdata->array_index);
-        }
+        /* Do the initial encoding */
+        apdu_len = rp_ack_encode_apdu_init(apdu, invoke_id, rpdata);
         /* propertyValue */
-        apdu_len += encode_opening_tag(&apdu[apdu_len], 3);
         for (len = 0; len < rpdata->application_data_len; len++) {
             apdu[apdu_len++] = rpdata->application_data[len];
         }
