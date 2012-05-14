236970d5de416adb14a20f481df4ab7105df23f0
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2012-05-14 16:48:03 +0000

    tagged

---
+++ include/datalink.h
@@ -0,0 +1,154 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2005 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef DATALINK_H
+#define DATALINK_H
+
+#include "config.h"
+#include "bacdef.h"
+
+#if defined(BACDL_ETHERNET)
+#include "ethernet.h"
+
+#define datalink_init ethernet_init
+#define datalink_send_pdu ethernet_send_pdu
+#define datalink_receive ethernet_receive
+#define datalink_cleanup ethernet_cleanup
+#define datalink_get_broadcast_address ethernet_get_broadcast_address
+#define datalink_get_my_address ethernet_get_my_address
+
+#elif defined(BACDL_ARCNET)
+#include "arcnet.h"
+
+#define datalink_init arcnet_init
+#define datalink_send_pdu arcnet_send_pdu
+#define datalink_receive arcnet_receive
+#define datalink_cleanup arcnet_cleanup
+#define datalink_get_broadcast_address arcnet_get_broadcast_address
+#define datalink_get_my_address arcnet_get_my_address
+
+#elif defined(BACDL_MSTP)
+#include "dlmstp.h"
+
+#define datalink_init dlmstp_init
+#define datalink_send_pdu dlmstp_send_pdu
+#define datalink_receive dlmstp_receive
+#define datalink_cleanup dlmstp_cleanup
+#define datalink_get_broadcast_address dlmstp_get_broadcast_address
+#define datalink_get_my_address dlmstp_get_my_address
+
+#elif defined(BACDL_BIP)
+#include "bip.h"
+#include "bvlc.h"
+
+#define datalink_init bip_init
+#if defined(BBMD_ENABLED) && BBMD_ENABLED
+#define datalink_send_pdu bvlc_send_pdu
+#define datalink_receive bvlc_receive
+#else
+#define datalink_send_pdu bip_send_pdu
+#define datalink_receive bip_receive
+#endif
+#define datalink_cleanup bip_cleanup
+#define datalink_get_broadcast_address bip_get_broadcast_address
+#ifdef BAC_ROUTING
+extern void routed_get_my_address(
+    BACNET_ADDRESS * my_address);
+#define datalink_get_my_address routed_get_my_address
+#else
+#define datalink_get_my_address bip_get_my_address
+#endif
+
+#else /* Ie, BACDL_ALL */
+#include "npdu.h"
+
+#define MAX_HEADER (8)
+#define MAX_MPDU (MAX_HEADER+MAX_PDU)
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    int datalink_send_pdu(
+        BACNET_ADDRESS * dest,
+        BACNET_NPDU_DATA * npdu_data,
+        uint8_t * pdu,
+        unsigned pdu_len);
+    extern uint16_t datalink_receive(
+        BACNET_ADDRESS * src,
+        uint8_t * pdu,
+        uint16_t max_pdu,
+        unsigned timeout);
+    extern void datalink_cleanup(
+        void);
+    extern void datalink_get_broadcast_address(
+        BACNET_ADDRESS * dest);
+    extern void datalink_get_my_address(
+        BACNET_ADDRESS * my_address);
+    extern void datalink_set_interface(
+        char *ifname);
+    extern void datalink_set(
+        char *datalink_string);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif
+/** @defgroup DataLink The BACnet Network (DataLink) Layer
+ * <b>6 THE NETWORK LAYER </b><br>
+ * The purpose of the BACnet network layer is to provide the means by which 
+ * messages can be relayed from one BACnet network to another, regardless of 
+ * the BACnet data link technology in use on that network. Whereas the data 
+ * link layer provides the capability to address messages to a single device 
+ * or broadcast them to all devices on the local network, the network layer 
+ * allows messages to be directed to a single remote device, broadcast on a 
+ * remote network, or broadcast globally to all devices on all networks. 
+ * A BACnet Device is uniquely located by a network number and a MAC address.
+ * 
+ * Each client or server application must define exactly one of these
+ * DataLink settings, which will control which parts of the code will be built:
+ * - BACDL_ETHERNET -- for Clause 7 ISO 8802-3 ("Ethernet") LAN
+ * - BACDL_ARCNET   -- for Clause 8 ARCNET LAN
+ * - BACDL_MSTP     -- for Clause 9 MASTER-SLAVE/TOKEN PASSING (MS/TP) LAN
+ * - BACDL_BIP      -- for ANNEX J - BACnet/IP 
+ * - BACDL_ALL      -- Unspecified for the build, so the transport can be 
+ *                     chosen at runtime from among these choices.
+ * - Clause 10 POINT-TO-POINT (PTP) and Clause 11 EIA/CEA-709.1 ("LonTalk") LAN
+ *   are not currently supported by this project.
+                      *//** @defgroup DLTemplates DataLink Template Functions
+ * @ingroup DataLink
+ * Most of the functions in this group are function templates which are assigned
+ * to a specific DataLink network layer implementation either at compile time or
+ * at runtime.
+ */
+#endif
---
+++ include/wp.h
@@ -0,0 +1,117 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2005 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef WRITEPROPERTY_H
+#define WRITEPROPERTY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "bacdcode.h"
+#include "bacapp.h"
+
+/** @note: write property can have application tagged data, or context tagged data,
+   or even complex data types (i.e. opening and closing tag around data).
+   It could also have more than one value or element.  */
+
+typedef struct BACnet_Write_Property_Data {
+    BACNET_OBJECT_TYPE object_type;
+    uint32_t object_instance;
+    BACNET_PROPERTY_ID object_property;
+    int32_t array_index;        /* use BACNET_ARRAY_ALL when not setting */
+    uint8_t application_data[MAX_APDU];
+    int application_data_len;
+    uint8_t priority;   /* use BACNET_NO_PRIORITY if no priority */
+    BACNET_ERROR_CLASS error_class;
+    BACNET_ERROR_CODE error_code;
+} BACNET_WRITE_PROPERTY_DATA;
+
+/** Attempts to write a new value to one property for this object type
+ *  of a given instance.
+ * A function template; @see device.c for assignment to object types.
+ * @ingroup ObjHelpers
+ *
+ * @param wp_data [in] Pointer to the BACnet_Write_Property_Data structure,
+ *                     which is packed with the information from the WP request.
+ * @return The length of the apdu encoded or -1 for error or
+ *         -2 for abort message.
+ */
+typedef bool(
+    *write_property_function) (
+    BACNET_WRITE_PROPERTY_DATA * wp_data);
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    /* encode service */
+    int wp_encode_apdu(
+        uint8_t * apdu,
+        uint8_t invoke_id,
+        BACNET_WRITE_PROPERTY_DATA * wp_data);
+
+    /* decode the service request only */
+    int wp_decode_service_request(
+        uint8_t * apdu,
+        unsigned apdu_len,
+        BACNET_WRITE_PROPERTY_DATA * wp_data);
+
+#ifdef TEST
+#include "ctest.h"
+    int wp_decode_apdu(
+        uint8_t * apdu,
+        unsigned apdu_len,
+        uint8_t * invoke_id,
+        BACNET_WRITE_PROPERTY_DATA * wp_data);
+
+    void testWriteProperty(
+        Test * pTest);
+    void testWritePropertyTag(
+        Test * pTest,
+        BACNET_APPLICATION_DATA_VALUE * value);
+#endif
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+/** @defgroup DSWP Data Sharing - Write Property Service (DS-WP)
+ * @ingroup DataShare
+ * 15.9 WriteProperty Service <br>
+ * The WriteProperty service is used by a client BACnet-user to modify the
+ * value of a single specified property of a BACnet object. This service
+ * potentially allows write access to any property of any object, whether a
+ * BACnet-defined object or not. Some implementors may wish to restrict write
+ * access to certain properties of certain objects. In such cases, an attempt
+ * to modify a restricted property shall result in the return of an error of
+ * 'Error Class' PROPERTY and 'Error Code' WRITE_ACCESS_DENIED.
+ */
+#endif
