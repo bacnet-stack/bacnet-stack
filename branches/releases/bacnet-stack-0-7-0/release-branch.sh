e572427aac93dc87b74e55cf4ec06231ebe272a6
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2012-05-22 21:25:18 +0000

    Backported 0.6.0 release branch script fixes.

--- branches/releases/bacnet-stack-0-7-0/release-branch.sh
+++ branches/releases/bacnet-stack-0-7-0/release-branch.sh
@@ -8,6 +8,7 @@ FRS_URL=skarg,bacnet@frs.sourceforge.net:/home/frs/project/b/ba/bacnet/bacnet-st
 if [ -z "$1" ] && [ -z "$2" ]
 then
   echo "Usage: `basename $0` 0.7.0 0.7.1"
+  echo "Use dotted version as [branch-revision] [tag-revision]."
   echo "Creates the ChangeLog."
   echo "Creates the release files."
   echo "Tags this branch release in subversion."
@@ -19,7 +20,7 @@ BRANCH_VERSION_DOTTED="$1"
 BRANCH_VERSION_DASHED="$(echo "$1" | sed 's/[\.*]/-/g')"
 TAGGED_VERSION_DOTTED="$2"
 TAGGED_VERSION_DASHED="$(echo "$2" | sed 's/[\.*]/-/g')"
-echo "Creating the ${TAGGED_VERSION_DOTTED} release files for $(BRANCH_VERSION_DOTTED)"
+echo "Creating the ${TAGGED_VERSION_DOTTED} release files for ${BRANCH_VERSION_DOTTED}"
 
 CHANGELOG=ChangeLog-${TAGGED_VERSION_DOTTED}
 echo "Creating the ${PROJECT} change log ${CHANGELOG}"
@@ -28,12 +29,13 @@ then
 rm ${CHANGELOG}
 fi
 svn update
-svn log --xml --verbose | xsltproc svn2cl.xsl - > ${CHANGELOG} > /dev/null
+svn log --xml --verbose | xsltproc svn2cl.xsl - > ${CHANGELOG}
 if [ -e "${CHANGELOG}" ]
 then
-echo "${CHANGELOG} created."
+  echo "${CHANGELOG} created."
 else
-echo "Failed to create ${CHANGELOG}"
+  echo "Failed to create ${CHANGELOG}"
+  exit 1
 fi
 
 BRANCH_NAME=${SVN_MODULE}-${BRANCH_VERSION_DASHED}
@@ -44,56 +46,65 @@ SVN_BASE_URL=https://${PROJECT}.svn.sourceforge.net/svnroot/${PROJECT}
 SVN_BRANCH_NAME=${SVN_BASE_URL}/branches/releases/${BRANCH_NAME}
 SVN_TAGGED_NAME=${SVN_BASE_URL}/tags/${TAGGED_NAME}
 echo "Setting a tag on the ${SVN_MODULE} module called ${TAGGED_NAME}"
-SVN_MESSAGE="Created version ${ARCHIVE_NAME}"
-svn copy ${SVN_BRANCH_NAME} ${SVN_TAGGED_NAME} -m ${SVN_MESSAGE} > /dev/null
+svn copy ${SVN_BRANCH_NAME} ${SVN_TAGGED_NAME} -m "tagged" > /dev/null
 echo "done."
 
-if [ -e "${ARCHIVE_NAME}" ]
+if [ -d "${ARCHIVE_NAME}" ]
 then
-rm -rf ${ARCHIVE_NAME}
+  echo "removing old ${ARCHIVE_NAME}..."
+  rm -rf ${ARCHIVE_NAME}
+  echo "done."
 fi
+
 echo "Getting a clean version out of subversion for Linux gzip"
 svn export ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
 echo "done."
+
 GZIP_FILENAME=${ARCHIVE_NAME}.tgz
+echo "tar and gzip the clean directory"
 if [ -e "${GZIP_FILENAME}" ]
 then
-rm ${GZIP_FILENAME}
+  echo "removing old ${GZIP_FILENAME}..."
+  rm ${GZIP_FILENAME}
+  echo "done."
 fi
-echo "tar and gzip the clean directory"
 tar -cvvzf ${GZIP_FILENAME} ${ARCHIVE_NAME}/ > /dev/null
 echo "done."
-
 if [ -e "${GZIP_FILENAME}" ]
 then
-echo "${GZIP_FILENAME} created."
+  echo "${GZIP_FILENAME} created."
 else
-echo "Failed to create ${GZIP_FILENAME}"
+  echo "Failed to create ${GZIP_FILENAME}"
+  exit 1
 fi
 
-echo "Removing the directory exported for Linux."
-rm -rf ${ARCHIVE_NAME}
-
-echo "Getting another clean version out of subversion for Windows zip"
-if [ -e "${ZIP_FILENAME}" ]
+if [ -d "${ARCHIVE_NAME}" ]
 then
-rm ${ZIP_FILENAME}
+  echo "removing old ${ARCHIVE_NAME}..."
+  rm -rf ${ARCHIVE_NAME}
+  echo "done."
 fi
+echo "Getting another clean version out of subversion for Windows zip"
 svn export --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
 ZIP_FILENAME=${ARCHIVE_NAME}.zip
 echo "done."
 echo "Zipping the directory exported for Windows."
 zip -r ${ZIP_FILENAME} ${ARCHIVE_NAME} > /dev/null
-
 if [ -e "${ZIP_FILENAME}" ]
 then
-echo "${ZIP_FILENAME} created."
+  echo "${ZIP_FILENAME} created."
 else
-echo "Failed to create ${ZIP_FILENAME}"
+  echo "Failed to create ${ZIP_FILENAME}"
+  exit 1
 fi
 
-echo "Removing the directory exported for Windows."
-rm -rf ${ARCHIVE_NAME}
+# remove SVN files
+if [ -d "${ARCHIVE_NAME}" ]
+then
+  echo "removing ${ARCHIVE_NAME}..."
+  rm -rf ${ARCHIVE_NAME}
+  echo "done."
+fi
 
 echo "Creating ${ARCHIVE_NAME}"
 mkdir ${ARCHIVE_NAME}
