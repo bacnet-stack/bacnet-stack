9eca5fc25e116052a15e18fb0e819eed6d88dc31
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2013-01-14 16:27:50 +0000

    Split error codes on write property for read-only known properties which gives write-access-denied, and unknown properties.

--- trunk/bacnet-stack/demo/object/csv.c
+++ trunk/bacnet-stack/demo/object/csv.c
@@ -423,9 +423,18 @@ bool CharacterString_Value_Write_Property(
                     object_instance, value.type.Boolean);
             }
             break;
-        default:
+        case PROP_OBJECT_IDENTIFIER:
+        case PROP_OBJECT_NAME:
+        case PROP_DESCRIPTION:
+        case PROP_OBJECT_TYPE:
+        case PROP_STATUS_FLAGS:
+        case PROP_EVENT_STATE:
             wp_data->error_class = ERROR_CLASS_PROPERTY;
             wp_data->error_code = ERROR_CODE_WRITE_ACCESS_DENIED;
+            break;
+        default:
+            wp_data->error_class = ERROR_CLASS_PROPERTY;
+            wp_data->error_code = ERROR_CODE_UNKNOWN_PROPERTY;
             break;
     }
 
