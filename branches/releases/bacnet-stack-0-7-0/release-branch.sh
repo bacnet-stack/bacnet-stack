c4c478857453edc61801a12633fda6df9a274882
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2013-03-13 21:50:24 +0000

    updated branch release script for new SVN URLs.

--- branches/releases/bacnet-stack-0-7-0/release-branch.sh
+++ branches/releases/bacnet-stack-0-7-0/release-branch.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Release helper for this project
 
+USERNAME=skarg
 PROJECT=bacnet
+SVN_BASE_URL=https://svn.code.sf.net/p/${PROJECT}/code
 SVN_MODULE=bacnet-stack
-FRS_URL=skarg,bacnet@frs.sourceforge.net:/home/frs/project/b/ba/bacnet/bacnet-stack
+FRS_URL=${USERNAME},${PROJECT}@frs.sourceforge.net:/home/frs/project/b/ba/bacnet/bacnet-stack
 
 if [ -z "$1" ] && [ -z "$2" ]
 then
@@ -41,12 +43,12 @@ fi
 BRANCH_NAME=${SVN_MODULE}-${BRANCH_VERSION_DASHED}
 ARCHIVE_NAME=${SVN_MODULE}-${TAGGED_VERSION_DOTTED}
 TAGGED_NAME=${SVN_MODULE}-${TAGGED_VERSION_DASHED}
-SVN_BASE_URL=https://${PROJECT}.svn.sourceforge.net/svnroot/${PROJECT}
-
-SVN_BRANCH_NAME=${SVN_BASE_URL}/branches/releases/${BRANCH_NAME}
 SVN_TAGGED_NAME=${SVN_BASE_URL}/tags/${TAGGED_NAME}
+SVN_BRANCH_NAME=${SVN_BASE_URL}/branches/releases/${BRANCH_NAME}
+
 echo "Setting a tag on the ${SVN_MODULE} module called ${TAGGED_NAME}"
-svn copy ${SVN_BRANCH_NAME} ${SVN_TAGGED_NAME} -m "tagged" > /dev/null
+TAG_COMMENT="Created version ${ARCHIVE_NAME}"
+svn copy --username=${USERNAME} ${SVN_BRANCH_NAME} ${SVN_TAGGED_NAME} -m "${TAG_COMMENT}"
 echo "done."
 
 if [ -d "${ARCHIVE_NAME}" ]
@@ -57,7 +59,7 @@ then
 fi
 
 echo "Getting a clean version out of subversion for Linux gzip"
-svn export ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
+svn export --username=${USERNAME} ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
 echo "done."
 
 GZIP_FILENAME=${ARCHIVE_NAME}.tgz
@@ -85,11 +87,12 @@ then
   echo "done."
 fi
 echo "Getting another clean version out of subversion for Windows zip"
-svn export --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
+svn export --username=${USERNAME} --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
 ZIP_FILENAME=${ARCHIVE_NAME}.zip
 echo "done."
 echo "Zipping the directory exported for Windows."
 zip -r ${ZIP_FILENAME} ${ARCHIVE_NAME} > /dev/null
+
 if [ -e "${ZIP_FILENAME}" ]
 then
   echo "${ZIP_FILENAME} created."
