dc7236cecdd2719eefe524483d32ee05b60663bd
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2013-03-13 21:42:47 +0000

    updated the release-branch script.

--- bacnet-stack-0-6-0/release-branch.sh
+++ bacnet-stack-0-6-0/release-branch.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 # Release helper for this project
 
+USERNAME=skarg
 PROJECT=bacnet
+SVN_BASE_URL=https://svn.code.sf.net/p/${PROJECT}/code
+SVN_BRANCH_NAME=${SVN_BASE_URL}/branches/releases/bacnet-stack-0-6-0
 SVN_MODULE=bacnet-stack
-FRS_URL=skarg,bacnet@frs.sourceforge.net:/home/frs/project/b/ba/bacnet/bacnet-stack
+FRS_URL=${USERNAME},${PROJECT}@frs.sourceforge.net:/home/frs/project/b/ba/bacnet/bacnet-stack
 
-if [ -z "$1" ] && [ -z "$2" ]
+if [ -z "$1" ]
 then
-  echo "Usage: `basename $0` 0.6.0 0.6.2"
-  echo "Use dotted version as [branch-revision] [tag-revision]."
+  echo "Usage: `basename $0` 0.0.0"
   echo "Creates the ChangeLog."
   echo "Creates the release files."
-  echo "Tags this branch release in subversion."
+  echo "Tags the current version in subversion."
   exit 1
 fi
 
 # convert 0.0.0 to 0-0-0
-BRANCH_VERSION_DOTTED="$1"
-BRANCH_VERSION_DASHED="$(echo "$1" | sed 's/[\.*]/-/g')"
-TAGGED_VERSION_DOTTED="$2"
-TAGGED_VERSION_DASHED="$(echo "$2" | sed 's/[\.*]/-/g')"
-echo "Creating the ${TAGGED_VERSION_DOTTED} release files for ${BRANCH_VERSION_DOTTED}"
+DOTTED_VERSION="$1"
+DASHED_VERSION="$(echo "$1" | sed 's/[\.*]/-/g')"
+echo "Creating the release files for version ${DOTTED_VERSION}"
 
-CHANGELOG=ChangeLog-${TAGGED_VERSION_DOTTED}
+CHANGELOG=ChangeLog-${DOTTED_VERSION}
 echo "Creating the ${PROJECT} change log ${CHANGELOG}"
 if [ -e "${CHANGELOG}" ]
 then
@@ -38,15 +38,13 @@ else
   exit 1
 fi
 
-BRANCH_NAME=${SVN_MODULE}-${BRANCH_VERSION_DASHED}
-ARCHIVE_NAME=${SVN_MODULE}-${TAGGED_VERSION_DOTTED}
-TAGGED_NAME=${SVN_MODULE}-${TAGGED_VERSION_DASHED}
-SVN_BASE_URL=https://${PROJECT}.svn.sourceforge.net/svnroot/${PROJECT}
+ARCHIVE_NAME=${SVN_MODULE}-${DOTTED_VERSION}
+TAGGED_NAME=${SVN_MODULE}-${DASHED_VERSION}
 
-SVN_BRANCH_NAME=${SVN_BASE_URL}/branches/releases/${BRANCH_NAME}
 SVN_TAGGED_NAME=${SVN_BASE_URL}/tags/${TAGGED_NAME}
 echo "Setting a tag on the ${SVN_MODULE} module called ${TAGGED_NAME}"
-svn copy ${SVN_BRANCH_NAME} ${SVN_TAGGED_NAME} -m "tagged" > /dev/null
+TAG_COMMENT="Created version ${ARCHIVE_NAME}"
+svn copy --username=${USERNAME} ${SVN_BRANCH_NAME} ${SVN_TAGGED_NAME} -m "${TAG_COMMENT}"
 echo "done."
 
 if [ -d "${ARCHIVE_NAME}" ]
@@ -57,7 +55,7 @@ then
 fi
 
 echo "Getting a clean version out of subversion for Linux gzip"
-svn export ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
+svn export --username=${USERNAME} ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
 echo "done."
 
 GZIP_FILENAME=${ARCHIVE_NAME}.tgz
@@ -85,11 +83,12 @@ then
   echo "done."
 fi
 echo "Getting another clean version out of subversion for Windows zip"
-svn export --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
+svn export --username=${USERNAME} --native-eol CRLF ${SVN_TAGGED_NAME} ${ARCHIVE_NAME} > /dev/null
 ZIP_FILENAME=${ARCHIVE_NAME}.zip
 echo "done."
 echo "Zipping the directory exported for Windows."
 zip -r ${ZIP_FILENAME} ${ARCHIVE_NAME} > /dev/null
+
 if [ -e "${ZIP_FILENAME}" ]
 then
   echo "${ZIP_FILENAME} created."
