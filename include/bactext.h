2758b676538f4fb21bd45079c19a6f89442edcf9
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2013-09-23 20:12:41 +0000

    Created version bacnet-stack-0.6.3

---
+++ include/bactext.h
@@ -0,0 +1,130 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2005 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef BACTEXT_H
+#define BACTEXT_H
+
+/* tiny implementations have no need to print */
+#if PRINT_ENABLED
+#define BACTEXT_PRINT_ENABLED
+#else
+#ifdef TEST
+#define BACTEXT_PRINT_ENABLED
+#endif
+#endif
+
+#ifdef BACTEXT_PRINT_ENABLED
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "indtext.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    const char *bactext_confirmed_service_name(
+        unsigned index);
+    const char *bactext_unconfirmed_service_name(
+        unsigned index);
+    const char *bactext_application_tag_name(
+        unsigned index);
+    const char *bactext_object_type_name(
+        unsigned index);
+    bool bactext_object_type_index(
+        const char *search_name,
+        unsigned *found_index);
+    const char *bactext_property_name(
+        unsigned index);
+    bool bactext_property_index(
+        const char *search_name,
+        unsigned *found_index);
+    const char *bactext_engineering_unit_name(
+        unsigned index);
+    bool bactext_engineering_unit_index(
+        const char *search_name,
+        unsigned *found_index);
+    const char *bactext_reject_reason_name(
+        unsigned index);
+    const char *bactext_abort_reason_name(
+        unsigned index);
+    const char *bactext_error_class_name(
+        unsigned index);
+    const char *bactext_error_code_name(
+        unsigned index);
+    unsigned bactext_property_id(
+        const char *name);
+    const char *bactext_month_name(
+        unsigned index);
+    const char *bactext_week_of_month_name(
+        unsigned index);
+    const char *bactext_day_of_week_name(
+        unsigned index);
+    const char *bactext_event_state_name(
+        unsigned index);
+    const char *bactext_binary_present_value_name(
+        unsigned index);
+    const char *bactext_binary_polarity_name(
+        unsigned index);
+    bool bactext_binary_present_value_index(
+        const char *search_name,
+        unsigned *found_index);
+    const char *bactext_reliability_name(
+        unsigned index);
+    const char *bactext_device_status_name(
+        unsigned index);
+    const char *bactext_segmentation_name(
+        unsigned index);
+    const char *bactext_node_type_name(
+        unsigned index);
+
+    const char *bactext_event_transition_name(
+        unsigned index);
+    bool bactext_event_transition_index(
+        const char *search_name,
+        unsigned *found_index);
+
+    const char *bactext_days_of_week_name(
+        unsigned index);
+    bool bactext_days_of_week_index(
+        const char *search_name,
+        unsigned *found_index);
+
+    const char *bactext_network_layer_msg_name(
+        unsigned index);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif /* BACTEXT_PRINT_ENABLED */
+#endif
---
+++ include/whohas.h
@@ -0,0 +1,92 @@
+/*####COPYRIGHTBEGIN####
+ -------------------------------------------
+ Copyright (C) 2006 Steve Karg
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to:
+ The Free Software Foundation, Inc.
+ 59 Temple Place - Suite 330
+ Boston, MA  02111-1307, USA.
+
+ As a special exception, if other files instantiate templates or
+ use macros or inline functions from this file, or you compile
+ this file and link it with other works to produce a work based
+ on this file, this file does not by itself cause the resulting
+ work to be covered by the GNU General Public License. However
+ the source code for this file must still be made available in
+ accordance with section (3) of the GNU General Public License.
+
+ This exception does not invalidate any other reasons why a work
+ based on this file might be covered by the GNU General Public
+ License.
+ -------------------------------------------
+####COPYRIGHTEND####*/
+#ifndef WHOHAS_H
+#define WHOHAS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "bacstr.h"
+
+typedef struct BACnet_Who_Has_Data {
+    int32_t low_limit;  /* deviceInstanceRange */
+    int32_t high_limit;
+    bool is_object_name;        /* true if a string */
+    union {
+        BACNET_OBJECT_ID identifier;
+        BACNET_CHARACTER_STRING name;
+    } object;
+} BACNET_WHO_HAS_DATA;
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+/* encode service  - use -1 for limit if you want unlimited */
+    int whohas_encode_apdu(
+        uint8_t * apdu,
+        BACNET_WHO_HAS_DATA * data);
+
+    int whohas_decode_service_request(
+        uint8_t * apdu,
+        unsigned apdu_len,
+        BACNET_WHO_HAS_DATA * data);
+
+    int whohas_decode_apdu(
+        uint8_t * apdu,
+        unsigned apdu_len,
+        BACNET_WHO_HAS_DATA * data);
+
+#ifdef TEST
+#include "ctest.h"
+    void testWhoHas(
+        Test * pTest);
+#endif
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+/** @defgroup DMDOB Device Management-Dynamic Object Binding (DM-DOB)
+ * @ingroup RDMS
+ * 16.9 Who-Has and I-Have Services <br>
+ * The Who-Has service is used by a sending BACnet-user to identify the device
+ * object identifiers and network addresses of other BACnet devices whose local
+ * databases contain an object with a given Object_Name or a given Object_Identifier.
+ * The I-Have service is used to respond to Who-Has service requests or to
+ * advertise the existence of an object with a given Object_Name or
+ * Object_Identifier. The I-Have service request may be issued at any time and
+ * does not need to be preceded by the receipt of a Who-Has service request.
+ * The Who-Has and I-Have services are unconfirmed services.
+ *
+ */
+#endif
