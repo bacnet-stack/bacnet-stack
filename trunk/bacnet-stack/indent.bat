eca6b095fd2f540200208452e7a36326a9e97ab5
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2013-10-29 15:08:01 +0000

    modified indent bat file to work correctly.

--- trunk/bacnet-stack/indent.bat
+++ trunk/bacnet-stack/indent.bat
@@ -1,6 +1,6 @@
 rem Indent the C and H files with specific coding standard
 rem requires 'indent.exe' from MSYS (MinGW).
-rem requires 'todos.exe' from MSYS (MinGW).
+rem See http://www.gnu.org/software/indent/manual/indent.pdf
 set OPTIONS=-kr -nut -nlp -ip4 -cli4 -bfda -nbc -nbbo -c0 -cd0 -cp0 -di0 -l79 -nhnl
 rem -kr The Kernighan & Ritchie style, corresponds to the following options:
 rem -nbad -bap -bbo -nbc -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0
@@ -26,12 +26,10 @@ goto :eof
 :treeProcess
 rem perform the indent on all the files of this subdirectory:
 for %%f in (*.c) do (
-    todos.exe "%%f"
-    indent.exe %OPTIONS% "%%f" -o "%%f"
+    indent.exe "%%f" -o "%%f" %OPTIONS%
 )
 for %%f in (*.h) do (
-    todos.exe "%%f"
-    indent.exe %OPTIONS% "%%f" -o "%%f"
+    indent.exe "%%f" -o "%%f" %OPTIONS%
 )
 rem loop over all directories and sub directories
 for /D %%d in (*) do (
