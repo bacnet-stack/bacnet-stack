9fb6b9107ab3dc9340313b9c77723bb4d6ca917b
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2013-12-09 20:31:47 +0000

    Fixed various compiler warnings.  Thank you, Morten Kvistgaard!

--- bacnet-stack/demo/ptransfer/main.c
+++ bacnet-stack/demo/ptransfer/main.c
@@ -62,7 +62,7 @@
 #define _stricmp stricmp
 #endif
 
-uint8_t Send_Private_Transfer_Request(
+extern uint8_t Send_Private_Transfer_Request(
     uint32_t device_id,
     uint16_t vendor_id,
     uint32_t service_number,
@@ -176,7 +176,6 @@ int main(
     int iCount = 0;
     int iType = 0;
     int iKey;
-    static int iLimit[3] = { 7, 11, 7 };
 
     if (((argc != 2) && (argc != 3)) || ((argc >= 2) &&
             (strcmp(argv[1], "--help") == 0))) {
@@ -300,7 +299,7 @@ int main(
                             NewData.cMyByte1 = iCount;
                             NewData.cMyByte2 = 255 - iCount;
                             NewData.fMyReal = (float) iCount;
-                            strcpy(NewData.sMyString, "Test Data - [x]");
+                            strcpy((char*)NewData.sMyString, "Test Data - [x]");
                             NewData.sMyString[13] = 'a' + iCount;
                             printf("Sending block %d\n", iCount);
                             invoke_id =
