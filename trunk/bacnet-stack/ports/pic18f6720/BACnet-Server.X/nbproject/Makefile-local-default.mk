7aa8ea7abc350bc360eac478ca111e41404e62a1
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2014-06-15 00:26:14 +0000

    added BACAPP_MINIMAL to PIC project

--- trunk/bacnet-stack/ports/pic18f6720/BACnet-Server.X/nbproject/Makefile-local-default.mk
+++ trunk/bacnet-stack/ports/pic18f6720/BACnet-Server.X/nbproject/Makefile-local-default.mk
@@ -14,24 +14,23 @@
 # You can invoke make with the values of the macros:
 # $ makeMP_CC="/opt/microchip/mplabc30/v3.30c/bin/pic30-gcc" ...  
 #
-SHELL=cmd.exe
-PATH_TO_IDE_BIN=C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/
+PATH_TO_IDE_BIN=/opt/microchip/mplabx/mplab_ide/mplab_ide/modules/../../bin/
 # Adding MPLAB X bin directory to path.
-PATH:=C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
+PATH:=/opt/microchip/mplabx/mplab_ide/mplab_ide/modules/../../bin/:$(PATH)
 # Path to java used to run MPLAB X when this makefile was created
-MP_JAVA_PATH="C:\Program Files (x86)\Microchip\MPLABX\sys\java\jre1.6.0_32-windows-x64\java-windows/bin/"
+MP_JAVA_PATH="/opt/microchip/mplabx/sys/java/jre1.6.0_33/bin/"
 OS_CURRENT="$(shell uname -s)"
-MP_CC="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin\mcc18.exe"
+MP_CC="/opt/microchip/mplabc18/v3.40/bin/mcc18"
 # MP_CPPC is not defined
 # MP_BC is not defined
-MP_AS="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin\..\mpasm\MPASMWIN.exe"
-MP_LD="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin\mplink.exe"
-MP_AR="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin\mplib.exe"
-DEP_GEN=${MP_JAVA_PATH}java -jar "C:/Program Files (x86)/Microchip/MPLABX/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar" 
-MP_CC_DIR="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin"
+MP_AS="/opt/microchip/mplabc18/v3.40/bin/../mpasm/MPASMWIN"
+MP_LD="/opt/microchip/mplabc18/v3.40/bin/mplink"
+MP_AR="/opt/microchip/mplabc18/v3.40/bin/mplib"
+DEP_GEN=${MP_JAVA_PATH}java -jar "/opt/microchip/mplabx/mplab_ide/mplab_ide/modules/../../bin/extractobjectdependencies.jar" 
+MP_CC_DIR="/opt/microchip/mplabc18/v3.40/bin"
 # MP_CPPC_DIR is not defined
 # MP_BC_DIR is not defined
-MP_AS_DIR="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin\..\mpasm"
-MP_LD_DIR="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin"
-MP_AR_DIR="C:\Program Files (x86)\Microchip\mplabc18\v3.40\bin"
+MP_AS_DIR="/opt/microchip/mplabc18/v3.40/bin/../mpasm"
+MP_LD_DIR="/opt/microchip/mplabc18/v3.40/bin"
+MP_AR_DIR="/opt/microchip/mplabc18/v3.40/bin"
 # MP_BC_DIR is not defined
