079e4e56353e67e5b80fca3f6bbf2841d19999ec
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2015-10-16 20:12:11 +0000

    Added Self/TT to statistics (was just Self for Tokens to Self) to track Tardy Tokens.  Tardy Tokens (TT) are Token Frames that are send out of order, from a MAC to was not the previous Token Frame MAC destination. Added Wireshark extcap command line parameter interface to mstpcap utility. Tested with Wireshark on Windows (mostly working). To use extcap, run Wireshark and go to the About-dialog.  Find a tab located there named "Folders". Locate the extcap search path. Copy the mstpcap.exe to that folder, which may not exist. Restart Wireshark, and look for "BACnet MS/TP on COMx" interfaces. Configure the interface to change baud rate. Capture directly from the interface.

--- bacnet-stack-0-8-0/ports/win32/rs485.h
+++ bacnet-stack-0-8-0/ports/win32/rs485.h
@@ -68,7 +68,10 @@ extern "C" {
     void RS485_Print_Error(
         void);
 
-    bool RS485_Interface_Valid(unsigned port_number);
+    bool RS485_Interface_Valid(
+        unsigned port_number);
+    void RS485_Print_Ports(
+        void);
 
 
 #ifdef __cplusplus
