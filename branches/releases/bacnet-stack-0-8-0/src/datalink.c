f92182b006e66d500fda704f6736b469111c5b5a
skarg <skarg@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2017-09-02 18:10:15 +0000

    Added BACDL_NONE for testing without a physical datalink.

--- branches/releases/bacnet-stack-0-8-0/src/datalink.c
+++ branches/releases/bacnet-stack-0-8-0/src/datalink.c
@@ -31,17 +31,16 @@
  License.
  -------------------------------------------
 ####COPYRIGHTEND####*/
+/** @file datalink.c  Optional run-time assignment of datalink transport */
+#include "datalink.h"
+
+#if defined(BACDL_ALL) || defined FOR_DOXYGEN
 #include "ethernet.h"
 #include "bip.h"
 #include "bvlc.h"
 #include "arcnet.h"
 #include "dlmstp.h"
-#include "datalink.h"
 #include <string.h>
-
-/** @file datalink.c  Optional run-time assignment of datalink transport */
-
-#if defined(BACDL_ALL) || defined FOR_DOXYGEN
 /* Function pointers - point to your datalink */
 
 /** Function template to Initialize the DataLink services at the given interface.
@@ -129,5 +128,50 @@ void datalink_set(
         datalink_get_broadcast_address = dlmstp_get_broadcast_address;
         datalink_get_my_address = dlmstp_get_my_address;
     }
+}
+#endif
+
+#if defined(BACDL_NONE)
+int datalink_send_pdu(
+    BACNET_ADDRESS * dest,
+    BACNET_NPDU_DATA * npdu_data,
+    uint8_t * pdu,
+    unsigned pdu_len)
+{
+    return 0;
+}
+
+uint16_t datalink_receive(
+    BACNET_ADDRESS * src,
+    uint8_t * pdu,
+    uint16_t max_pdu,
+    unsigned timeout)
+{
+    return 0;
+}
+
+void datalink_cleanup(
+    void)
+{
+}
+
+void datalink_get_broadcast_address(
+    BACNET_ADDRESS * dest)
+{
+}
+
+void datalink_get_my_address(
+    BACNET_ADDRESS * my_address)
+{
+}
+
+void datalink_set_interface(
+    char *ifname)
+{
+}
+
+void datalink_set(
+    char *datalink_string)
+{
 }
 #endif
