e3fab4a96c5bec179051c2166c6bf1758830ad44
fchaxel <fchaxel@1e814d3a-8a88-4260-be86-c0dc25669c1e>
2017-11-22 07:28:36 +0000

    esp32 support

---
+++ bacnet-stack/ports/esp32/src/bip_init.c
@@ -0,0 +1,57 @@
+//
+// Copyleft  F.Chaxel 2017
+//
+
+#include "esp_log.h"
+#include "esp_wifi.h"
+
+#include "lwip/sockets.h"
+#include "lwip/netdb.h"
+
+#include "bip.h"
+
+long bip_getaddrbyname(
+    const char *host_name)
+{
+    return 0;
+}
+
+void bip_set_interface(char *ifname)
+{
+}
+
+void bip_cleanup (void)
+{
+    close(bip_socket());
+    bip_set_socket(-1);
+}
+
+bool bip_init(char *ifname)
+{
+    
+    tcpip_adapter_ip_info_t ip_info = { 0 };
+
+    int value = 1;
+
+    tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &ip_info);
+
+    bip_set_interface(ifname);    
+    bip_set_port(htons(0xBAC0));
+    bip_set_addr(ip_info.ip.addr);
+    bip_set_broadcast_addr((ip_info.ip.addr&ip_info.netmask.addr)|(~ip_info.netmask.addr));
+
+    int  sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    struct sockaddr_in saddr = { 0 };
+
+    saddr.sin_family = PF_INET;
+    saddr.sin_port = htons(0xBAC0);
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    bind(sock, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
+
+    setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (char *) &value, sizeof(value));
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (char *) &value, sizeof(value));
+
+    bip_set_socket(sock);
+
+    return true;
+}
