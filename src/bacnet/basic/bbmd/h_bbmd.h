e4048b0a39970766f36b5a06b9da8c2548269685
Steve Karg <steve.karg@legrand.us>
2020-04-03 17:30:50 -0500

    adorn the function prototypes with export to fix cmake build.

--- src/bacnet/basic/bbmd/h_bbmd.h
+++ src/bacnet/basic/bbmd/h_bbmd.h
@@ -32,6 +32,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include "bacnet/bacnet_stack_exports.h"
 #include "bacnet/bacdef.h"
 #include "bacnet/datalink/bvlc.h"
 
@@ -40,32 +41,42 @@ extern "C" {
 #endif /* __cplusplus */
 
 /* user application function prototypes */
+BACNET_STACK_EXPORT
 int bvlc_handler(BACNET_IP_ADDRESS *addr,
     BACNET_ADDRESS *src,
     uint8_t *npdu,
     uint16_t npdu_len);
 
+BACNET_STACK_EXPORT
 int bvlc_send_pdu(BACNET_ADDRESS *dest,
     BACNET_NPDU_DATA *npdu_data,
     uint8_t *pdu,
     unsigned pdu_len);
 
+BACNET_STACK_EXPORT
 uint16_t bvlc_get_last_result(void);
 
+BACNET_STACK_EXPORT
 uint8_t bvlc_get_function_code(void);
 
+BACNET_STACK_EXPORT
 void bvlc_maintenance_timer(uint16_t seconds);
 
+BACNET_STACK_EXPORT
 void bvlc_init(void);
 
+BACNET_STACK_EXPORT
 void bvlc_debug_enable(void);
 
 /* send a Read BDT request */
+BACNET_STACK_EXPORT
 int bvlc_bbmd_read_bdt(BACNET_IP_ADDRESS *bbmd_addr);
 /* send a Read FDT request */
+BACNET_STACK_EXPORT
 int bvlc_bbmd_read_fdt(BACNET_IP_ADDRESS *bbmd_addr);
 
 /* registers with a bbmd as a foreign device */
+BACNET_STACK_EXPORT
 int bvlc_register_with_bbmd(
     BACNET_IP_ADDRESS *address, uint16_t time_to_live_seconds);
 
@@ -75,28 +86,34 @@ int bvlc_register_with_bbmd(
  */
 
 /* Get broadcast distribution table list */
+BACNET_STACK_EXPORT
 BACNET_IP_BROADCAST_DISTRIBUTION_TABLE_ENTRY *bvlc_bdt_list(void);
 
 /* Invalidate all entries in the broadcast distribution table */
+BACNET_STACK_EXPORT
 void bvlc_bdt_list_clear(void);
 
 /* Backup broadcast distribution table to a file.
  * Filename is the BBMD_BACKUP_FILE constant
  */
+BACNET_STACK_EXPORT
 void bvlc_bdt_backup_local(void);
 
 /* Restore broadcast distribution from a file.
  * Filename is the BBMD_BACKUP_FILE constant
  */
+BACNET_STACK_EXPORT
 void bvlc_bdt_restore_local(void);
 
 /* Set global IP address of a NAT enabled router which is used in forwarded
  * messages. Enables NAT handling.
  */
+BACNET_STACK_EXPORT
 void bvlc_set_global_address_for_nat(const BACNET_IP_ADDRESS *addr);
 
 /* Disable NAT handling of BBMD.
  */
+BACNET_STACK_EXPORT
 void bvlc_disable_nat(void);
 
 #ifdef __cplusplus
