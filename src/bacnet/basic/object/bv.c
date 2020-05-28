9137531eb0807c961c4f0049209629d0e4bc0b39
Carlos Gomes Martinho <kmartinho8@gmail.com>
2020-05-28 15:56:58 +0200

    fix: compiler warnings Wtautological-pointer-compare (#93)
    warning: comparison of array 'wp_data->application_data' equal to a null pointer is always false [-Wtautological-pointer-compare]

--- src/bacnet/basic/object/bv.c
+++ src/bacnet/basic/object/bv.c
@@ -422,8 +422,7 @@ bool Binary_Value_Write_Property(BACNET_WRITE_PROPERTY_DATA *wp_data)
     if (wp_data == NULL) {
         return false;
     }
-    if ((wp_data->application_data == NULL) ||
-        (wp_data->application_data_len == 0)) {
+    if (wp_data->application_data_len == 0) {
         return false;
     }
 
