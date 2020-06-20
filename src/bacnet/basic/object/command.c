2c12929365f52e389a5cbad5b8ab08c1a91c3a7a
thorrockstar <postmaster@overthehill.de>
2020-06-20 21:24:02 +0200

    Added missing return value check for bitstrings.

--- src/bacnet/basic/object/command.c
+++ src/bacnet/basic/object/command.c
@@ -261,6 +261,9 @@ int cl_decode_apdu(uint8_t *apdu,
         case BACNET_APPLICATION_TAG_BIT_STRING:
             len = decode_context_bitstring(
                 &apdu[dec_len], 4, &bcl->Value.type.Bit_String);
+            if (len < 0) {
+                return BACNET_STATUS_REJECT;
+            }
             break;
         case BACNET_APPLICATION_TAG_ENUMERATED:
             len = decode_context_enumerated(
