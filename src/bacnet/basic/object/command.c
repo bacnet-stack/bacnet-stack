f942167c0d0c8af080feac7dcfca6a115c5c051a
thorrockstar <postmaster@overthehill.de>
2020-06-20 20:05:35 +0200

    Added missing return value check for booleans.

--- src/bacnet/basic/object/command.c
+++ src/bacnet/basic/object/command.c
@@ -227,6 +227,9 @@ int cl_decode_apdu(uint8_t *apdu,
         case BACNET_APPLICATION_TAG_BOOLEAN:
             len = decode_context_boolean2(
                 &apdu[dec_len], 4, &bcl->Value.type.Boolean);
+            if (len < 0) {
+                return BACNET_STATUS_REJECT;
+            }
             break;
         case BACNET_APPLICATION_TAG_UNSIGNED_INT:
             len = decode_context_unsigned(
