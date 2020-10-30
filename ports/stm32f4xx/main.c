06dde5050c84c93adc872dba2b4c7c8c53673f5d
Steve Karg <steve@kargs.net>
2020-10-30 11:53:57 -0500

    Add MSTP port to nucleof429zi using DFR0259 RS485 shield (#136)
    Co-authored-by: Steve Karg <skarg@users.sourceforge.net>

---
+++ ports/stm32f4xx/main.c
@@ -0,0 +1,65 @@
+/************************************************************************
+ *
+ * Copyright (C) 2011 Steve Karg <skarg@users.sourceforge.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *************************************************************************/
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "stm32f4xx_pwr.h"
+#include "stm32f4xx_rcc.h"
+#include "system_stm32f4xx.h"
+#include "bacnet/basic/sys/mstimer.h"
+#include "rs485.h"
+#include "led.h"
+#include "bacnet.h"
+
+int main(void)
+{
+    struct mstimer Blink_Timer;
+
+    /*At this stage the microcontroller clock setting is already configured,
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+       system_stm32f4xx.c file */
+    SystemCoreClockUpdate();
+    /* enable some clocks - USART and GPIO clocks are enabled in our drivers */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    /* enable our hardware */
+    mstimer_init();
+    led_init();
+    rs485_init();
+    bacnet_init();
+    mstimer_set(&Blink_Timer, 125);
+    for (;;) {
+        if (mstimer_expired(&Blink_Timer)) {
+            mstimer_reset(&Blink_Timer);
+            led_toggle(LED_LD3);
+            led_toggle(LED_RS485);
+        }
+        led_task();
+        bacnet_task();
+    }
+}
