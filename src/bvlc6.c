da87090a7851f28e62b765f9003809013d030963
Steve Karg <skarg@users.sourceforge.net>
2022-03-29 18:54:24 -0500

    fix non utf-8 characters in source code

--- src/bvlc6.c
+++ src/bvlc6.c
@@ -1403,9 +1403,9 @@ int bvlc6_decode_secure_bvll(
  *
  * This message provides a mechanism whereby a foreign device
  * shall cause a BBMD to distribute a Forwarded-NPDU
- * BVLC to the local multicast domain, to all BBMD’s configured
- * in the BBMD’s BDT, and to all foreign devices in the
- * BBMD’s FDT.
+ * BVLC to the local multicast domain, to all BBMDs configured
+ * in the BBMDs BDT, and to all foreign devices in the
+ * BBMDs FDT.
  *
  * @param pdu - buffer to store the encoding
  * @param pdu_size - size of the buffer to store encoding
