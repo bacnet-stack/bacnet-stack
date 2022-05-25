4612812653a9de400db70ad4ad67014b9b65226d
Ondřej Hruška <ondra@ondrovo.com>
2022-05-25 17:32:19 +0200

    Add setters for BIP_DL_Debug (BBMD registration debug logging)

--- src/bacnet/datalink/dlenv.h
+++ src/bacnet/datalink/dlenv.h
@@ -40,6 +40,14 @@ extern "C" {
         void);
 
     BACNET_STACK_EXPORT
+    void bip_dl_debug_enable(
+        void);
+
+    BACNET_STACK_EXPORT
+    void bip_dl_debug_disable(
+        void);
+
+    BACNET_STACK_EXPORT
     int dlenv_register_as_foreign_device(
         void);
 
