d47e3d36024afba30002489217a59924d5f8670a
Steve Karg <skarg@users.sourceforge.net>
2022-08-01 17:19:16 -0500

    remove key.c from any projects.  Add unit test from key.c to keylist.

--- test/bacnet/basic/sys/keylist/src/main.c
+++ test/bacnet/basic/sys/keylist/src/main.c
@@ -284,6 +284,35 @@ static void testKeyListLarge(void)
 
     return;
 }
+
+/* test the encode and decode macros */
+static void testKeySample(void)
+{
+    int type, id;
+    int type_list[] = { 0, 1, KEY_TYPE_MAX / 2, KEY_TYPE_MAX - 1, -1 };
+    int id_list[] = { 0, 1, KEY_ID_MAX / 2, KEY_ID_MAX - 1, -1 };
+    int type_index = 0;
+    int id_index = 0;
+    int decoded_type, decoded_id;
+    KEY key;
+
+    while (type_list[type_index] != -1) {
+        while (id_list[id_index] != -1) {
+            type = type_list[type_index];
+            id = id_list[id_index];
+            key = KEY_ENCODE(type, id);
+            decoded_type = KEY_DECODE_TYPE(key);
+            decoded_id = KEY_DECODE_ID(key);
+            zassert_equal(decoded_type, type, NULL);
+            zassert_equal(decoded_id, id, NULL);
+            id_index++;
+        }
+        id_index = 0;
+        type_index++;
+    }
+
+    return;
+}
 /**
  * @}
  */
@@ -296,7 +325,8 @@ void test_main(void)
      ztest_unit_test(testKeyListFILO),
      ztest_unit_test(testKeyListDataKey),
      ztest_unit_test(testKeyListDataIndex),
-     ztest_unit_test(testKeyListLarge)
+     ztest_unit_test(testKeyListLarge),
+     ztest_unit_test(testKeySample)
      );
 
     ztest_run_test_suite(keylist_tests);
