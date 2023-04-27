070166e9edf41eb5c27ba2dff15b7ed1dfe4dbba
Kirill Neznamov <kirill.neznamov@dsr-corporation.com>
2023-04-27 16:56:36 +0300

    protect libwebsockets calls using global mutex for bsd and windows platforms

--- ports/win32/websocket-cli.c
+++ ports/win32/websocket-cli.c
@@ -450,7 +450,9 @@ static DWORD WINAPI bws_cli_worker(LPVOID arg)
             if (conn->want_send_data) {
                 DEBUG_PRINTF(
                     "bws_cli_worker() process request for sending data\n");
+                bsc_websocket_global_lock();
                 lws_callback_on_writable(conn->ws);
+                bsc_websocket_global_unlock();
             }
         } else if (conn->state == BSC_WEBSOCKET_STATE_DISCONNECTING) {
             DEBUG_PRINTF("bws_cli_worker() process disconnecting event\n");
@@ -667,9 +669,10 @@ BSC_WEBSOCKET_RET bws_cli_connect(BSC_WEBSOCKET_PROTOCOL proto,
     bws_cli_conn[h].state = BSC_WEBSOCKET_STATE_CONNECTING;
     bws_cli_conn[h].err_code = ERROR_CODE_SUCCESS;
     *out_handle = h;
+    bsc_websocket_global_lock();
     lws_client_connect_via_info(&cinfo);
+    bsc_websocket_global_unlock();
     bsc_mutex_unlock(&bws_cli_mutex);
-
     DEBUG_PRINTF("bws_cli_connect() <<< ret = %d\n", BSC_WEBSOCKET_SUCCESS);
     return BSC_WEBSOCKET_SUCCESS;
 }
@@ -743,8 +746,10 @@ BSC_WEBSOCKET_RET bws_cli_dispatch_send(
         return BSC_WEBSOCKET_INVALID_OPERATION;
     }
 
+    bsc_websocket_global_lock();
     written =
         lws_write(bws_cli_conn[h].ws, payload, payload_size, LWS_WRITE_BINARY);
+    bsc_websocket_global_unlock();
 
     DEBUG_PRINTF("bws_cli_dispatch_send() %d bytes is sent\n", written);
 
