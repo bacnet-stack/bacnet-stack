fbef56751b49309e7863d1e7b54ec5f6cdaf93eb
Steve Karg <skarg@users.sourceforge.net>
2023-07-19 11:06:40 -0500

    [WIP] improve test coverage for objects

--- test/bacnet/basic/object/trendlog/src/main.c
+++ test/bacnet/basic/object/trendlog/src/main.c
@@ -8,7 +8,7 @@
  */
 
 #include <zephyr/ztest.h>
-#include <bacnet/basic/object/schedule.h>
+#include <bacnet/basic/object/trendlog.h>
 #include <bacnet/bactext.h>
 
 /**
@@ -19,7 +19,7 @@
 /**
  * @brief Test
  */
-static void testSchedule(void)
+static void test_Trend_Log_ReadProperty(void)
 {
     uint8_t apdu[MAX_APDU] = { 0 };
     int len = 0;
@@ -33,22 +33,22 @@ static void testSchedule(void)
     unsigned count = 0;
     bool status = false;
 
-    Schedule_Init();
-    count = Schedule_Count();
+    Trend_Log_Init();
+    count = Trend_Log_Count();
     zassert_true(count > 0, NULL);
     rpdata.application_data = &apdu[0];
     rpdata.application_data_len = sizeof(apdu);
-    rpdata.object_type = OBJECT_SCHEDULE;
-    rpdata.object_instance = Schedule_Index_To_Instance(0);;
-    status = Schedule_Valid_Instance(rpdata.object_instance);
+    rpdata.object_type = OBJECT_TRENDLOG;
+    rpdata.object_instance = Trend_Log_Index_To_Instance(0);;
+    status = Trend_Log_Valid_Instance(rpdata.object_instance);
     zassert_true(status, NULL);
-    Schedule_Property_Lists(&pRequired, &pOptional, &pProprietary);
+    Trend_Log_Property_Lists(&pRequired, &pOptional, &pProprietary);
     while ((*pRequired) != -1) {
         rpdata.object_property = *pRequired;
         rpdata.array_index = BACNET_ARRAY_ALL;
-        len = Schedule_Read_Property(&rpdata);
-        zassert_not_equal(len, BACNET_STATUS_ERROR, NULL);
+        len = Trend_Log_Read_Property(&rpdata);
         if (len > 0) {
+            zassert_not_equal(len, BACNET_STATUS_ERROR, NULL);
             test_len = bacapp_decode_application_data(rpdata.application_data,
                 (uint8_t)rpdata.application_data_len, &value);
             if (len != test_len) {
@@ -69,7 +69,7 @@ static void testSchedule(void)
     while ((*pOptional) != -1) {
         rpdata.object_property = *pOptional;
         rpdata.array_index = BACNET_ARRAY_ALL;
-        len = Schedule_Read_Property(&rpdata);
+        len = Trend_Log_Read_Property(&rpdata);
         zassert_not_equal(len, BACNET_STATUS_ERROR, NULL);
         if (len > 0) {
             test_len = bacapp_decode_application_data(rpdata.application_data,
@@ -92,7 +92,8 @@ static void testSchedule(void)
 
 void test_main(void)
 {
-    ztest_test_suite(schedule_tests, ztest_unit_test(testSchedule));
+    ztest_test_suite(trendlog_tests, 
+        ztest_unit_test(test_Trend_Log_ReadProperty));
 
-    ztest_run_test_suite(schedule_tests);
+    ztest_run_test_suite(trendlog_tests);
 }
