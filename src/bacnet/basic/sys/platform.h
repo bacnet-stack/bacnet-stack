7b45b3b35863bd86d9ac925916ea33b59e944683
Steve Karg <skarg@users.sourceforge.net>
2024-02-08 15:32:57 -0600

    fixes for MSVC snprintf from C99

--- src/bacnet/basic/sys/platform.h
+++ src/bacnet/basic/sys/platform.h
@@ -40,17 +40,43 @@
 #   define BACNET_STACK_DEPRECATED(message)
 # endif
 
-#if defined(WIN32) || defined(WIN64)
-#include <strings.h>
+#if defined(_MSC_VER)
 #ifndef strcasecmp
 #define strcasecmp _stricmp
 #endif
 #ifndef strncasecmp
 #define strncasecmp _strnicmp
 #endif
-#include<stdio.h>
-#ifndef snprintf
-#define snprintf _snprintf
+#if (_MSC_VER < 1900)
+#include <stdio.h>
+#include <stdarg.h>
+#define snprintf c99_snprintf
+#define vsnprintf c99_vsnprintf
+
+__inline int c99_vsnprintf(char *outBuf, size_t size, const char *format,
+    va_list ap)
+{
+    int count = -1;
+
+    if (size != 0)
+        count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+    if (count == -1)
+        count = _vscprintf(format, ap);
+
+    return count;
+}
+
+__inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...)
+{
+    int count;
+    va_list ap;
+
+    va_start(ap, format);
+    count = c99_vsnprintf(outBuf, size, format, ap);
+    va_end(ap);
+
+    return count;
+}
 #endif
 #elif defined(__ZEPHYR__)
 #  include <strings.h>
