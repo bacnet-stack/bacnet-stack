fd6f63dcfa726ab559e7115568fae0c0afee1129
Steve Karg <skarg@users.sourceforge.net>
2024-03-26 13:58:09 -0500

    file header comment cleanup

--- ports/zephyr/bacnet-config.h
+++ ports/zephyr/bacnet-config.h
@@ -1,28 +1,14 @@
-/**************************************************************************
-*
-* Copyright (c) 2024 Legrand North America, LLC.
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be included
-* in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*
-*********************************************************************/
-
+/**
+ * @file
+ * @brief Port specific configuration for BACnet Stack for Zephyr OS
+ * @author Steve Karg
+ * @date 2024
+ * @section LICENSE
+ * 
+ * Copyright (C) 2024 Steve Karg <skarg@users.sourceforge.net>
+ * 
+ * SPDX-License-Identifier: MIT
+ */
 #ifndef BACNET_PORTS_ZEPHYR_BACNET_CONFIG_H
 #define BACNET_PORTS_ZEPHYR_BACNET_CONFIG_H
 
@@ -30,7 +16,8 @@
 #error bacnet-config.h included outside of BACNET_CONFIG_H control
 #endif
 
-#include <zephyr/sys/util.h>  // Provides platform-specific defn of ARRAY_SIZE()
+/* provides platform specific define for ARRAY_SIZE */
+#include <zephyr/sys/util.h>
 
 /* some smaller defaults for microcontrollers */
 #if !defined(MAX_TSM_TRANSACTIONS)
@@ -54,4 +41,4 @@
 #define MAX_FD_ENTRIES 5
 #endif
 
-#endif // BACNET_PORTS_ZEPHYR_BACNET_CONFIG_H
+#endif
