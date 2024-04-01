38e2ebf17397f8ba57efaa85e4225dd05e0be340
Steve Karg <skarg@users.sourceforge.net>
2024-04-01 13:58:37 -0500

    Bugfix/zephyr defines kconfig (#610)
    * Added COV features to time-value object
    
    * Added zephyr config names with BACNET prefix

--- ports/zephyr/bacnet-config.h
+++ ports/zephyr/bacnet-config.h
@@ -19,26 +19,4 @@
 /* provides platform specific define for ARRAY_SIZE */
 #include <zephyr/sys/util.h>
 
-/* some smaller defaults for microcontrollers */
-#if !defined(MAX_TSM_TRANSACTIONS)
-#define MAX_TSM_TRANSACTIONS 1
-#endif
-#if !defined(MAX_ADDRESS_CACHE)
-#define MAX_ADDRESS_CACHE 1
-#endif
-#if !defined(MAX_CHARACTER_STRING_BYTES)
-#define MAX_CHARACTER_STRING_BYTES 64
-#endif
-#if !defined(MAX_OCTET_STRING_BYTES)
-#define MAX_OCTET_STRING_BYTES 64
-#endif
-
-/* K.6.6 BIBB - Network Management-BBMD Configuration-B (NM-BBMDC-B)*/
-#if !defined(MAX_BBMD_ENTRIES)
-#define MAX_BBMD_ENTRIES 5
-#endif
-#if !defined(MAX_FD_ENTRIES)
-#define MAX_FD_ENTRIES 5
-#endif
-
 #endif
