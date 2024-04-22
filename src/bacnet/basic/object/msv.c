c5b97bb4f447fc19517a8733ecb85c358aed2169
darren conroy <SESA745259@se.com>
2024-04-22 15:32:29 +0000

    Just prints for MSV init

--- src/bacnet/basic/object/msv.c
+++ src/bacnet/basic/object/msv.c
@@ -389,7 +389,7 @@ bool Multistate_Value_Set_State_text_init(MSV_STATE_TEXT_INIT_OPTIONS_LIST *pIni
     for (i = 0; i < MSV_Max_Index; i++) {
 
         for(j = option_index; j < pInit_state_text_data->length; j++) {
-            strncpy(State_Text[i][j][k], pInit_state_text_data->MSV_State_Text_Init_Objects[j].option, sizeof(State_Text[i][j][k]));
+         //   strncpy(State_Text[i][j][k], pInit_state_text_data->MSV_State_Text_Init_Objects[j].option, sizeof(State_Text[i][j][k]));
             PRINTF("@@@@@@@@@@@@@@@@@ STATE INIT %u \r\n", j);
             if(pInit_state_text_data->MSV_State_Text_Init_Objects[j].state_text_option_index != i) {
                 option_index = j;
