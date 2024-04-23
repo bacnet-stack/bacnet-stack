7e1ff8b45e93efcd3a4d2930a3fa286a3ece8e5f
darren conroy <SESA745259@se.com>
2024-04-23 00:59:37 -0700

    MSV init set state text testing

--- src/bacnet/basic/object/msv.c
+++ src/bacnet/basic/object/msv.c
@@ -62,7 +62,7 @@ static char Object_Name[MAX_MULTISTATE_VALUES][MAX_CHARACTER_STRING_BYTES];
 /* object description storage */
 static char Object_Description[MAX_MULTISTATE_VALUES][MAX_CHARACTER_STRING_BYTES];
 /* object state text storage */
-static char State_Text[MAX_MULTISTATE_VALUES][MULTISTATE_NUMBER_OF_STATES][64];
+static char State_Text[MAX_MULTISTATE_VALUES][MULTISTATE_NUMBER_OF_STATES][100];
 /* Here is out Instance */
 static uint32_t Instance[MAX_MULTISTATE_VALUES];
 
@@ -392,9 +392,9 @@ bool Multistate_Value_Set_State_text_init(MSV_STATE_TEXT_INIT_OPTIONS_LIST *pIni
 
         for(j = option_index; j < pInit_state_text_data->length; j++) {
             k++;
-            strncpy(State_Text[i][j], pInit_state_text_data->MSV_State_Text_Init_Objects[j].option, 64);
+            strncpy(State_Text[i][j], pInit_state_text_data->MSV_State_Text_Init_Objects[j].option, sizeof(State_Text[0][0]));
             //memcpy(State_Text[i][j][k], pInit_state_text_data->MSV_State_Text_Init_Objects[j].option, strlen(State_Text[i][j][k]));
-            PRINTF("@@@@@@@@@@@@@@@@@ STATE INIT %u %S \r\n", j,State_Text[i][j]);
+            PRINTF("@@@@@@@@@@@@@@@@@ STATE INIT %u %s \r\n", j,State_Text[i][j]);
             if(pInit_state_text_data->MSV_State_Text_Init_Objects[j].state_text_option_index != i) {
                 option_index = j -1;
                 k = 0;
