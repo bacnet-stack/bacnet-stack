3363b0bd9c53ca4bd2cf5d0ad2067bda4f42de43
darren conroy <SESA745259@se.com>
2024-07-08 06:16:43 -0700

    removing prints

--- ports/linux/dlmstp.c
+++ ports/linux/dlmstp.c
@@ -649,7 +649,6 @@ uint8_t dlmstp_max_master(void)
 /* RS485 Baud Rate 9600, 19200, 38400, 57600, 115200 */
 void dlmstp_set_baud_rate(uint32_t baud)
 {
-    fprintf(stderr, "bacnet-stack baud rate %u \r\n", baud);
     RS485_Set_Baud_Rate(baud);
 }
 
