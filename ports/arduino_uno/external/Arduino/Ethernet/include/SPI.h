0fc35505debc7802de7a88fa1952f6961d3a6399
Kari Argillander <kari.argillander@fidelix.com>
2024-08-12 20:44:31 +0300

    Change misc license texts to SPDX-License-Identifier
    There are some external code in repo which are not licenses as most of
    the stuff in this repo. We still want every file to have SPDX identifier
    to easily grep licenses.

--- ports/arduino_uno/external/Arduino/Ethernet/include/SPI.h
+++ ports/arduino_uno/external/Arduino/Ethernet/include/SPI.h
@@ -2,10 +2,7 @@
  * Copyright (c) 2010 by Cristian Maglie <c.maglie@bug.st>
  * SPI Master library for arduino.
  *
- * This file is free software; you can redistribute it and/or modify
- * it under the terms of either the GNU General Public License version 2
- * or the GNU Lesser General Public License version 2.1, both as
- * published by the Free Software Foundation.
+ * SPDX-License-Identifier: GPL-2.0-only OR LGPL-2.1-only
  */
 
 #ifndef _SPI_H_INCLUDED
