c81d28b4399fd7f98a373de920008071542a04e7
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2024-08-12 11:43:26 +0100

    Getter and setter functions for intrinsic reporting related properties

--- src/bacnet/basic/object/bi.h
+++ src/bacnet/basic/object/bi.h
@@ -192,13 +192,54 @@ extern "C" {
         BACNET_GET_ALARM_SUMMARY_DATA * getalarm_data);
 
     BACNET_STACK_EXPORT
+    uint32_t Binary_Input_Time_Delay(
+            uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
+    bool Binary_Input_Time_Delay_Set(
+            uint32_t object_instance,
+            uint32_t time_delay);
+
+    BACNET_STACK_EXPORT
+    uint32_t Binary_Input_Notification_Class(
+            uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
+    bool Binary_Input_Notification_Class_Set(
+            uint32_t object_instance,
+            uint32_t notification_class);
+
+    BACNET_STACK_EXPORT
+    BACNET_BINARY_PV Binary_Input_Alarm_Value(
+            uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
     bool Binary_Input_Alarm_Value_Set(
         uint32_t object_instance, BACNET_BINARY_PV value);
 
     BACNET_STACK_EXPORT
+    uint32_t Binary_Input_Event_Enable(
+            uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
+    bool Binary_Input_Event_Enable_Set(
+            uint32_t object_instance,
+            uint32_t event_enable);
+
+    BACNET_STACK_EXPORT
+    BACNET_NOTIFY_TYPE Binary_Input_Notify_Type(
+            uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
+    bool Binary_Input_Notify_Type_Set(
+            uint32_t object_instance,
+            BACNET_NOTIFY_TYPE notify_type);
+#endif
+
+    BACNET_STACK_EXPORT
     void Binary_Input_Intrinsic_Reporting(
         uint32_t object_instance);
-#endif
+
 
 
 #ifdef __cplusplus
