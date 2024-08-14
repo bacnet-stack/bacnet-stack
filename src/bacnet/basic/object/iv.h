c1195dd8a0d27a2f672d862e0aa6c22efba42520
Steve Karg <skarg@users.sourceforge.net>
2024-08-14 08:54:03 -0500

    Added CreateObject, DeleteObject, and COV to Integer Value object (#719)
    Co-authored-by: Tomasz Kazimierz Motyl <tomasz.motyl@se.com>

--- src/bacnet/basic/object/iv.h
+++ src/bacnet/basic/object/iv.h
@@ -71,16 +71,18 @@ extern "C" {
         uint32_t object_instance,
         BACNET_PROPERTY_VALUE * value_list);
     BACNET_STACK_EXPORT
-    float Integer_Value_COV_Increment(
-        uint32_t instance);
+    uint32_t Integer_Value_COV_Increment(
+        uint32_t object_instance);
     BACNET_STACK_EXPORT
     void Integer_Value_COV_Increment_Set(
-        uint32_t instance,
-        float value);
+            uint32_t object_instance,
+            uint32_t value);
+
 
     BACNET_STACK_EXPORT
-    char *Integer_Value_Description(
-        uint32_t instance);
+    bool Integer_Value_Description(
+        uint32_t object_instance,
+        BACNET_CHARACTER_STRING *description);
     BACNET_STACK_EXPORT
     bool Integer_Value_Description_Set(
         uint32_t instance,
@@ -105,6 +107,19 @@ extern "C" {
     BACNET_STACK_EXPORT
     void Integer_Value_Init(
         void);
+
+    BACNET_STACK_EXPORT
+    uint32_t Integer_Value_Create(
+        uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
+    bool Integer_Value_Delete(
+            uint32_t object_instance);
+
+    BACNET_STACK_EXPORT
+    void Integer_Value_Cleanup(void);
+
+
 
 #ifdef __cplusplus
 }
