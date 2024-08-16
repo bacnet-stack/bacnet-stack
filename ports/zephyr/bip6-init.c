e1002c433fa2d7238ad62c31bdc211631108cdbc
Steve Karg <skarg@users.sourceforge.net>
2024-08-16 09:02:24 -0500

    Revert "Zephyr 3.7.0 integration fixes: (#723) (#724)"
    This reverts commit 2d7a2961a239307d7807f7ad1fa050fd92d1c9b4.

--- ports/zephyr/bip6-init.c
+++ ports/zephyr/bip6-init.c
@@ -11,7 +11,6 @@
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
-#include <zephyr/net/net_if.h>
 #include <zephyr/net/net_ip.h>
 #include <zephyr/net/socket.h>
 #include <zephyr/net/socket_select.h>
