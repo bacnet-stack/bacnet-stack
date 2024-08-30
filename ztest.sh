622a9e609e4a4c6e1ff4e65121a25704822ea5c3
Steve Karg <skarg@users.sourceforge.net>
2024-08-30 11:01:18 -0500

    Moved west manifest, zephyr folder, and ports/zephyr folders to another repository bacnet-stack-zephyr (#757)

--- ztest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rm -rf twister-out.unit_testing
-../zephyr/scripts/twister -O twister-out.unit_testing -p unit_testing -T zephyr/tests
-#for file in CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
-#do
-#    find twister-out.unit_testing -name $file -exec rm -rf {} \; || true
-#done
