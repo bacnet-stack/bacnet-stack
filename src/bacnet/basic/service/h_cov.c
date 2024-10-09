3b780f7beedde13a68376e180d5372a24fe3a828
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2024-10-09 13:58:11 +0100

    Decreasing the buffer length from 48 to 40 bytes

--- src/bacnet/basic/service/h_cov.c
+++ src/bacnet/basic/service/h_cov.c
@@ -280,7 +280,7 @@ static int cov_encode_subscription(
  * 31 bytes for IPv4 (longest MAC) plus 3 extra bytes for the maximum length
  * of PID (5 bytes) and lets round it up to the 64bit machine word
  * alignment */
-#define MAX_COV_SUB_SIZE (48)
+#define MAX_COV_SUB_SIZE (40)
 int handler_cov_encode_subscriptions(uint8_t *apdu, int max_apdu)
 {
     if (apdu) {
