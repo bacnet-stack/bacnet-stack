a0c3b2425b408260da75e5d56113e3efee281c49
Steve Karg <skarg@users.sourceforge.net>
2024-10-18 07:24:22 -0500

    Fixed channel object breakage

--- src/bacnet/basic/object/channel.c
+++ src/bacnet/basic/object/channel.c
@@ -1543,7 +1543,8 @@ bool Channel_Write_Property(BACNET_WRITE_PROPERTY_DATA *wp_data)
     bool status = false; /* return value */
     int len = 0;
     BACNET_APPLICATION_DATA_VALUE value = { 0 };
-    bool is_array;
+    int element_len = 0;
+    uint32_t count = 0;
 
     /* decode the some of the request */
     len = bacapp_decode_application_data(
