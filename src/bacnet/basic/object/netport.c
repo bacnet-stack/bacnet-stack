4a55d21d1d30b49543c23f789f333bf29912578f
darren conroy <SESA745259@se.com>
2024-10-22 04:40:09 -0700

    correcting fprintf mistake

--- src/bacnet/basic/object/netport.c
+++ src/bacnet/basic/object/netport.c
@@ -731,7 +731,7 @@ bool Network_Port_MAC_Address(
     uint32_t object_instance, BACNET_OCTET_STRING *mac_address)
 {
     bool status = false;
-    fprintf("Network_Port_MAC_Address mac_address %d\n", mac_address);
+    fprintf(stderr, "Network_Port_MAC_Address mac_address %d\n", mac_address);
     if (mac_address) {
         uint8_t mac_tmp[4 + 2] = { 0 };
         uint8_t mac_len = Network_Port_MAC_Address_Value(
