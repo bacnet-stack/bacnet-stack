410ea9d5b0a576bccbca9eedcc17e8c7a870a7ca
darren conroy <SESA745259@se.com>
2024-11-11 04:02:29 -0800

    for git build changes clang

--- src/bacnet/basic/object/ai.c
+++ src/bacnet/basic/object/ai.c
@@ -393,7 +393,8 @@ const char *Analog_Input_Name_ASCII(uint32_t object_instance)
  *
  * @return  event-state property value
  */
-unsigned Analog_Input_Event_State(uint32_t object_instance) {
+unsigned Analog_Input_Event_State(uint32_t object_instance)
+{
     unsigned state = EVENT_STATE_NORMAL;
 #if !defined(INTRINSIC_REPORTING)
     struct analog_input_descr *pObject;
@@ -420,7 +421,8 @@ unsigned Analog_Input_Event_State(uint32_t object_instance) {
  *
  * @return  event-detection-enable property value
  */
-bool Analog_Input_Event_Detection_Enable(uint32_t object_instance) {
+bool Analog_Input_Event_Detection_Enable(uint32_t object_instance) 
+{
     bool retval = false;
 #if !defined(INTRINSIC_REPORTING)
     (void)object_instance;
@@ -444,8 +446,9 @@ bool Analog_Input_Event_Detection_Enable(uint32_t object_instance) {
  *
  * @return  event-detection-enable property value
  */
-bool Analog_Input_Event_Detection_Enable_Set(uint32_t object_instance,
-                                             bool value) {
+bool Analog_Input_Event_Detection_Enable_Set(
+    uint32_t object_instance, bool value)
+{
     bool retval = false;
 #if !defined(INTRINSIC_REPORTING)
     (void)object_instance;
