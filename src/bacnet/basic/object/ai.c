1c2654401300e8bc6fe0de55bc46f4cb2fb02d71
dcmaverick <150046088+dcmaverick@users.noreply.github.com>
2024-11-11 16:21:34 +0000

    Merge pull request #81 from se-apc/dc_Analogs_Event_Detection_Enable
    Dc analogs event detection enable

--- src/bacnet/basic/object/ai.c
+++ src/bacnet/basic/object/ai.c
@@ -44,6 +44,7 @@ static const int Properties_Optional[] = {
     PROP_TIME_DELAY, PROP_NOTIFICATION_CLASS, PROP_HIGH_LIMIT,
     PROP_LOW_LIMIT, PROP_DEADBAND, PROP_LIMIT_ENABLE, PROP_EVENT_ENABLE,
     PROP_ACKED_TRANSITIONS, PROP_NOTIFY_TYPE, PROP_EVENT_TIME_STAMPS,
+    PROP_EVENT_DETECTION_ENABLE,
 #endif
     -1
 };
@@ -303,10 +304,11 @@ const char *Analog_Input_Name_ASCII(uint32_t object_instance)
 unsigned Analog_Input_Event_State(uint32_t object_instance)
 {
     unsigned state = EVENT_STATE_NORMAL;
-#if defined(INTRINSIC_REPORTING)
+#if !defined(INTRINSIC_REPORTING)
     struct analog_input_descr *pObject;
 
     pObject = Analog_Input_Object(object_instance);
+
     if (pObject) {
         state = pObject->Event_State;
     }
@@ -316,6 +318,52 @@ unsigned Analog_Input_Event_State(uint32_t object_instance)
 
     return state;
 }
+
+#if defined(INTRINSIC_REPORTING)
+/**
+ * For a given object instance-number, gets the event-detection-enable property
+ * value
+ *
+ * @param  object_instance - object-instance number of the object
+ *
+ * @return  event-detection-enable property value
+ */
+bool Analog_Input_Event_Detection_Enable(uint32_t object_instance)
+{
+    bool retval = false;
+
+    struct analog_input_descr *pObject = Analog_Input_Object(object_instance);
+
+    if (pObject) {
+        retval = pObject->Event_Detection_Enable;
+    }
+
+    return retval;
+}
+
+/**
+ * For a given object instance-number, sets the event-detection-enable property
+ * value
+ *
+ * @param  object_instance - object-instance number of the object
+ *
+ * @return  event-detection-enable property value
+ */
+bool Analog_Input_Event_Detection_Enable_Set(
+    uint32_t object_instance, bool value)
+{
+    bool retval = false;
+
+    struct analog_input_descr *pObject = Analog_Input_Object(object_instance);
+
+    if (pObject) {
+        pObject->Event_Detection_Enable = value;
+        retval = true;
+    }
+
+    return retval;
+}
+#endif
 
 /**
  * @brief For a given object instance-number, returns the description
@@ -760,6 +808,10 @@ int Analog_Input_Read_Property(BACNET_READ_PROPERTY_DATA *rpdata)
                                                                  : false);
             apdu_len = encode_application_bitstring(&apdu[0], &bit_string);
             break;
+        case PROP_EVENT_DETECTION_ENABLE:
+            apdu_len = encode_application_boolean(
+                &apdu[0], pObject->Event_Detection_Enable);
+            break;
         case PROP_ACKED_TRANSITIONS:
             bitstring_init(&bit_string);
             bitstring_set_bit(
@@ -1014,6 +1066,12 @@ void Analog_Input_Intrinsic_Reporting(uint32_t object_instance)
     if (!CurrentAI) {
         return;
     }
+
+    /* check whether Intrinsic reporting is enabled */
+    if (!CurrentAI->Event_Detection_Enable) {
+        return; /* limits are not configured */
+    }
+
     if (CurrentAI->Ack_notify_data.bSendAckNotify) {
         /* clean bSendAckNotify flag */
         CurrentAI->Ack_notify_data.bSendAckNotify = false;
@@ -1601,6 +1659,8 @@ uint32_t Analog_Input_Create(uint32_t object_instance)
             pObject->Changed = false;
             pObject->Event_State = EVENT_STATE_NORMAL;
 #if defined(INTRINSIC_REPORTING)
+            pObject->Event_Detection_Enable = true;
+            pObject->Time_Delay = 0;
             /* notification class not connected */
             pObject->Notification_Class = BACNET_MAX_INSTANCE;
             /* initialize Event time stamps using wildcards
