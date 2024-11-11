9ddf202b93b91302b9d2bcdd3ae2653dffb079b8
darren conroy <SESA745259@se.com>
2024-11-11 04:28:26 -0800

    debugging

--- src/bacnet/basic/object/ai.c
+++ src/bacnet/basic/object/ai.c
@@ -2096,8 +2096,8 @@ uint32_t Analog_Input_Create(uint32_t object_instance)
             pObject->Out_Of_Service = false;
             pObject->Changed = false;
             pObject->Event_State = EVENT_STATE_NORMAL;
-            pObject->Event_Detection_Enable = true;
 #if defined(INTRINSIC_REPORTING)
+            pObject->Event_Detection_Enable = true;
             pObject->Time_Delay = 0;
             /* notification class not connected */
             pObject->Notification_Class = BACNET_MAX_INSTANCE;
