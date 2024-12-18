389b6a7d4d2a5c90ebb1d6af6ea72c6b532ea3e4
Jonathan <74002342+jd-imi@users.noreply.github.com>
2024-12-18 18:40:23 +0100

    h_dcc.c : Return correct error when no service request (#877)

--- src/bacnet/dcc.c
+++ src/bacnet/dcc.c
@@ -250,7 +250,7 @@ int dcc_encode_apdu(
  * @param apdu  Pointer to the received request.
  * @param apdu_len_max  Valid count of bytes in the buffer.
  * @param timeDuration  Pointer to the duration given in minutes [optional]
- * @param enable_disable  Pointer to the variable takingthe communication
+ * @param enable_disable  Pointer to the variable taking the communication
  * enable/disable.
  * @param password  Pointer to the password [optional]
  *
