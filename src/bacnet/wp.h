c018822b9c6cb4e09e220460e01943904ecf1651
darren conroy <SESA745259@se.com>
2025-02-21 09:25:33 -0800

    selective manual merge from up-stream

--- src/bacnet/wp.h
+++ src/bacnet/wp.h
@@ -85,6 +85,9 @@ bool write_property_empty_string_valid(
     BACNET_WRITE_PROPERTY_DATA *wp_data,
     const BACNET_APPLICATION_DATA_VALUE *value,
     size_t len_max);
+BACNET_STACK_EXPORT
+    bool write_property_bacnet_array_valid(BACNET_WRITE_PROPERTY_DATA *wp_data);
+
 
 #ifdef __cplusplus
 }
