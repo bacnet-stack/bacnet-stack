4df896b820d83dae94ca320e78018ba0343b84c5
Steve Karg <skarg@users.sourceforge.net>
2025-03-13 15:51:37 -0500

    Feature/basic-program-object (#940)
    * Added basic Program object and unit test.
    
    * Integrate the basic Program object with example applications.

--- src/bacnet/bacenum.h
+++ src/bacnet/bacenum.h
@@ -1088,7 +1088,7 @@ typedef enum {
     MAX_POLARITY = 2
 } BACNET_POLARITY;
 
-typedef enum {
+typedef enum BACnetProgramRequest {
     PROGRAM_REQUEST_READY = 0,
     PROGRAM_REQUEST_LOAD = 1,
     PROGRAM_REQUEST_RUN = 2,
@@ -1097,7 +1097,7 @@ typedef enum {
     PROGRAM_REQUEST_UNLOAD = 5
 } BACNET_PROGRAM_REQUEST;
 
-typedef enum {
+typedef enum BACnetProgramState {
     PROGRAM_STATE_IDLE = 0,
     PROGRAM_STATE_LOADING = 1,
     PROGRAM_STATE_RUNNING = 2,
@@ -1106,18 +1106,17 @@ typedef enum {
     PROGRAM_STATE_UNLOADING = 5
 } BACNET_PROGRAM_STATE;
 
-typedef enum {
+typedef enum BACnetProgramError {
     PROGRAM_ERROR_NORMAL = 0,
     PROGRAM_ERROR_LOAD_FAILED = 1,
     PROGRAM_ERROR_INTERNAL = 2,
     PROGRAM_ERROR_PROGRAM = 3,
     PROGRAM_ERROR_OTHER = 4,
-    /* Enumerated values 0-63 are reserved for definition by ASHRAE.  */
-    /* Enumerated values 64-65535 may be used by others subject to  */
-    /* the procedures and constraints described in Clause 23. */
-    /* do the max range inside of enum so that
-       compilers will allocate adequate sized datatype for enum
-       which is used to store decoding */
+    PROGRAM_ERROR_RESERVED_MIN = 5,
+    PROGRAM_ERROR_RESERVED_MAX = 63,
+    /* Enumerated values 0-63 are reserved for definition by ASHRAE.
+       Enumerated values 64-65535 may be used by others subject
+       to the procedures and constraints described in Clause 23. */
     PROGRAM_ERROR_PROPRIETARY_MIN = 64,
     PROGRAM_ERROR_PROPRIETARY_MAX = 65535
 } BACNET_PROGRAM_ERROR;
