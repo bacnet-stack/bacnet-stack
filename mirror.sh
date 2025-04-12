cdf170f39b85389561e36c32b7deac7c3854026f
Steve Karg <skarg@users.sourceforge.net>
2025-04-12 14:59:52 -0500

    Improved mirror script debugging

--- mirror.sh
+++ mirror.sh
@@ -9,7 +9,10 @@ mirror_dest='ssh://skarg@git.code.sf.net/p/bacnet/src'
 mirror_tree=$(mktemp -d 2>/dev/null || mktemp -d -t 'bacnet-mirror')
 
 echo "Mirror the Github repository with Sourceforge.net"
+echo "Temporary directory for mirroring: $mirror_tree"
 cd $mirror_tree
+echo "Cloning from source: $mirror_source"
 git clone --bare $mirror_source .
+echo "Pushing to destination: $mirror_dest"
 git push --mirror $mirror_dest
 cd -
