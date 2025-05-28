84fe4a0cca9676243320afe6b01cc617788a49e6
Ryan Mulder <ryan.mulder@antrum.com>
2025-05-28 15:54:10 -0400

    avoid adding new source file

--- ports/linux/rs485.c
+++ ports/linux/rs485.c
@@ -396,6 +396,53 @@ void RS485_Print_Ports(void)
     }
 }
 
+int termios2_tcsetattr(
+    const int fildes,
+    int optional_actions,
+    const struct termios2 *const termios2_p)
+{
+    /* https://man7.org/linux/man-pages/man2/TCSETS.2const.html */
+
+    switch (optional_actions) {
+        case TCSANOW:
+            optional_actions = TCSETS2;
+            break;
+        case TCSADRAIN:
+            optional_actions = TCSETSW2;
+            break;
+        case TCSAFLUSH:
+            optional_actions = TCSETSF2;
+            break;
+        default:
+            errno = EINVAL;
+            return -1;
+    };
+    return ioctl(fildes, optional_actions, termios2_p);
+}
+
+int termios2_tcgetattr(const int fildes, struct termios2 *termios2_p)
+{
+    /* https://man7.org/linux/man-pages/man2/TCSETS.2const.html */
+    return ioctl(fildes, TCGETS2, termios2_p);
+}
+
+int termios2_tcflush(const int fildes, const int queue_selector)
+{
+    /* https://manpages.opensuse.org/Tumbleweed/man-pages/TCFLSH.2const.en.html
+     */
+    return ioctl(fildes, TCFLSH, queue_selector);
+}
+
+int termios2_tcdrain(const int fildes)
+{
+    /*
+    https://man7.org/linux/man-pages/man2/TCSBRK.2const.html
+    TCSBRK Equivalent to tcsendbreak(fd, arg).
+    Linux treats tcsendbreak(fd,arg) with nonzero arg like tcdrain(fd)
+    */
+    return ioctl(fildes, TCSBRK, 1);
+}
+
 #ifdef TEST_RS485
 #include <string.h>
 int main(int argc, char *argv[])
