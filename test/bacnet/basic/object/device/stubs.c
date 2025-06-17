8208b9e8e7dfec05c4f9de75e54570baee2b24f0
darren conroy <SESA745259@se.com>
2025-06-17 12:53:24 +0100

    added to datetime_sync to stubs

--- test/bacnet/basic/object/device/stubs.c
+++ test/bacnet/basic/object/device/stubs.c
@@ -18,6 +18,19 @@ void datetime_init(void)
 {
 }
 
+void datetime_timesync(
+    BACNET_DATE *bdate,
+    BACNET_TIME *btime,
+    bool utc)
+{
+    (void)bdate;
+    (void)btime;
+    (void)utc;
+
+    /* This is a stub, so we do nothing */
+}
+
+
 bool datetime_local(
     BACNET_DATE *bdate,
     BACNET_TIME *btime,
