71cf55f2bb05d2e34ac439565c0f6cbe17bde1eb
darren conroy <SESA745259@se.com>
2025-06-18 00:44:37 +0100

    more prints

--- ports/linux/datetime-init.c
+++ ports/linux/datetime-init.c
@@ -65,19 +65,24 @@ void datetime_timesync(BACNET_DATE *bdate, BACNET_TIME *btime, bool utc)
     tv_inp.tv_sec = mktime(timeinfo);
     tv_inp.tv_usec = btime->hundredths * 10000;
 
+    fprintf(stderr, "[%s %d] tv_inp: %ld.%06ld\n",
+        __FILE__, __LINE__, (long)tv_inp.tv_sec, (long)tv_inp.tv_usec);
     if (gettimeofday(&tv_sys, NULL) == 0) {
 
         if (utc) {
             Time_Offset = time_difference(tv_inp, tv_sys) -
                 (timezone - timeinfo->tm_isdst * 3600) * 1000;
+                    fprintf(stderr, "[%s %d] Time Offset = %d ms\n",
+            __FILE__, __LINE__, Time_Offset);
 
         } else {
             Time_Offset = time_difference(tv_inp, tv_sys);
+                    fprintf(stderr, "[%s %d] Time Offset = %d ms\n",
+            __FILE__, __LINE__, Time_Offset);
         }
         fprintf(stderr, "[%s %d] tv_sys: %ld.%06ld\n",
             __FILE__, __LINE__, (long)tv_sys.tv_sec, (long)tv_sys.tv_usec);
-        fprintf(stderr, "[%s %d] Time Offset = %d ms\n",
-            __FILE__, __LINE__, Time_Offset);
+
 #if PRINT_ENABLED
         printf("Time offset = %d\n", Time_Offset);
 #endif
