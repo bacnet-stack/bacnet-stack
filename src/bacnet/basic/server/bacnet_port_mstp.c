0f54a40cde127f035715603dba51763f872a3a9e
Steve Karg <skarg@users.sourceforge.net>
2025-06-29 10:26:46 -0500

    Changed bacnet-basic MS/TP port to only pull data from dlmstp module at initiatilzation.

--- src/bacnet/basic/server/bacnet_port_mstp.c
+++ src/bacnet/basic/server/bacnet_port_mstp.c
@@ -43,27 +43,21 @@ bool bacnet_port_mstp_init(void)
 {
     uint32_t instance = 1;
     BACNET_ADDRESS addr = { 0 };
-    long baud_rate = 38400;
-    long mac_address = 127;
-    uint8_t max_master = 127;
-    long max_info_frames = 1;
 
-    dlmstp_set_max_info_frames(max_info_frames);
-    dlmstp_set_max_master(max_master);
-    dlmstp_set_baud_rate(baud_rate);
-    dlmstp_set_mac_address(mac_address);
     Network_Port_Object_Instance_Number_Set(0, instance);
     Network_Port_Name_Set(instance, "BACnet MS/TP Port");
     Network_Port_Type_Set(instance, PORT_TYPE_MSTP);
     dlmstp_get_my_address(&addr);
     Network_Port_MAC_Address_Set(instance, &addr.mac[0], addr.mac_len);
-
     Network_Port_Reliability_Set(instance, RELIABILITY_NO_FAULT_DETECTED);
-    Network_Port_Link_Speed_Set(instance, baud_rate);
     Network_Port_Out_Of_Service_Set(instance, false);
     Network_Port_Quality_Set(instance, PORT_QUALITY_UNKNOWN);
     Network_Port_APDU_Length_Set(instance, MAX_APDU);
     Network_Port_Network_Number_Set(instance, 0);
+    /* MS/TP specific */
+    Network_Port_Link_Speed_Set(instance, dlmstp_baud_rate());
+    Network_Port_MSTP_Max_Info_Frames_Set(instance, dlmstp_max_info_frames());
+    Network_Port_MSTP_Max_Master_Set(instance, dlmstp_max_master());
     /* last thing - clear pending changes - we don't want to set these
        since they are already set */
     Network_Port_Changes_Pending_Set(instance, false);
