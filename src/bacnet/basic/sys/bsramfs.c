290f79d8d7dfc763a1732c911a73467ef5a8593b
Steve Karg <skarg@users.sourceforge.net>
2025-08-05 12:48:02 -0500

    Added static RAM file system to use with basic bacnet file object.

---
+++ src/bacnet/basic/sys/bsramfs.c
@@ -0,0 +1,235 @@
+/**
+ * @file
+ * @brief A static RAM file system BACnet File Object implementation.
+ * @author Steve Karg
+ * @date 2025
+ * @copyright SPDX-License-Identifier: MIT
+ */
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+/* BACnet Stack defines - first */
+#include "bacnet/bacdef.h"
+#include "bacnet/basic/object/bacfile.h"
+#include "bacnet/basic/sys/bsramfs.h"
+
+static struct bacnet_file_sramfs_data *File_List = NULL;
+
+bool bacfile_sramfs_add(struct bacnet_file_sramfs_data *file_data)
+{
+    struct bacnet_file_sramfs_data *head = NULL;
+
+    if (!file_data) {
+        return false; /* invalid data */
+    }
+    head = File_List;
+    if (!head) {
+        /* first file data */
+        File_List = file_data;
+    } else {
+        /* find the end of the list */
+        while (head->next) {
+            head = head->next;
+        }
+        /* add to the end of the list */
+        head->next = file_data;
+    }
+
+    return true;
+}
+
+/**
+ * @brief For a given object instance-number, returns the pathname
+ * @param  object_instance - object-instance number of the object
+ * @return  internal file system path and name, or NULL if not set
+ */
+static struct bacnet_file_sramfs_data *bacfile_sramfs_open(const char *pathname)
+{
+    struct bacnet_file_sramfs_data *head = NULL;
+    uint32_t crc32K;
+    int index;
+
+    if (!pathname || pathname[0] == 0) {
+        return NULL; /* invalid pathname */
+    }
+    head = File_List;
+    while (head) {
+        if (strcmp(head->pathname, pathname) == 0) {
+            return head; /* found the file */
+        }
+        head = head->next;
+    }
+
+    return head;
+}
+
+/**
+ * @brief Determines the file size for a given file
+ * @param  pathname - name of the file to get the size for
+ * @return  file size in bytes, or 0 if not found
+ */
+const char *bacfile_sramfs_file_data(const char *pathname)
+{
+    struct bacnet_file_sramfs_data *pFile;
+    const char *file_data = NULL;
+
+    pFile = bacfile_sramfs_open(pathname);
+    if (pFile) {
+        file_data = pFile->data;
+    }
+
+    return file_data;
+}
+
+/**
+ * @brief Determines the file size for a given file
+ * @param  pathname - name of the file to get the size for
+ * @return  file size in bytes, or 0 if not found
+ */
+size_t bacfile_sramfs_file_size(const char *pathname)
+{
+    struct bacnet_file_sramfs_data *pFile;
+    size_t file_size = 0;
+
+    pFile = bacfile_sramfs_open(pathname);
+    if (pFile) {
+        file_size = pFile->size;
+    }
+
+    return file_size;
+}
+
+/**
+ * @brief Reads stream data from a file
+ * @param pathname - name of the file to read from
+ * @param fileStartPosition - starting position in the file
+ * @param fileData - data buffer to read into
+ * @param fileDataLen - size of the data buffer
+ * @return number of bytes read, or 0 if not successful
+ */
+size_t bacfile_sramfs_read_stream_data(
+    const char *pathname,
+    int32_t fileStartPosition,
+    uint8_t *fileData,
+    size_t fileDataLen)
+{
+    struct bacnet_file_sramfs_data *pFile;
+    size_t len = 0;
+
+    pFile = bacfile_sramfs_open(pathname);
+    if (pFile) {
+        if (fileStartPosition + fileDataLen > pFile->size) {
+            /* read only up to the end of the file */
+            len = pFile->size - fileStartPosition;
+        } else {
+            len = fileDataLen;
+        }
+        memcpy(fileData, pFile->data + fileStartPosition, len);
+    }
+
+    return len;
+}
+
+/**
+ * @brief Count the number of records in a file
+ * @param records - string of null-terminated records
+ * @return number of records
+ */
+static size_t record_count(const char *records)
+{
+    size_t count = 0;
+    int len = 0;
+
+    if (records) {
+        do {
+            len = bacnet_strnlen(records, MAX_OCTET_STRING_BYTES);
+            if (len > 0) {
+                count++;
+                records = records + len + 1;
+            }
+        } while (len > 0);
+    }
+
+    return count;
+}
+
+/**
+ * @brief Get the specific record at index 0..N
+ * @param records - string of null-terminated records
+ * @param record_index - record index number 0..N of the records
+ * @return record, or NULL
+ */
+static char *record_by_index(char *records, size_t index)
+{
+    size_t count = 0;
+    int len = 0;
+
+    if (records) {
+        do {
+            len = bacnet_strnlen(records, MAX_OCTET_STRING_BYTES);
+            if (len > 0) {
+                if (index == count) {
+                    return records;
+                }
+                count++;
+                records = records + len + 1;
+            }
+        } while (len > 0);
+    }
+
+    return NULL;
+}
+
+/**
+ * @brief Reads record data from a file
+ * @param pathname - name of the file to read from
+ * @param fileStartRecord - starting record in the file
+ * @param fileIndexRecord - index of the record to read
+ * @param fileData - data buffer to read into
+ * @param fileDataLen - size of the data buffer
+ * @return true if successful, false otherwise
+ */
+bool bacfile_sramfs_read_record_data(
+    const char *pathname,
+    int32_t fileStartRecord,
+    size_t fileIndexRecord,
+    uint8_t *fileData,
+    size_t fileDataLen)
+{
+    bool status = false;
+    size_t fileSeekRecord;
+    struct bacnet_file_sramfs_data *pFile;
+    char *record;
+    size_t record_len;
+
+    pFile = bacfile_sramfs_open(pathname);
+    if (pFile) {
+        fileSeekRecord = fileStartRecord + fileIndexRecord;
+        /* seek to the start record */
+        record = record_by_index(pFile->data, fileSeekRecord);
+        if (record) {
+            record_len = bacnet_strnlen(record, MAX_OCTET_STRING_BYTES);
+            if ((record_len > 0) && (record_len <= fileDataLen)) {
+                /* copy the record data */
+                memcpy(fileData, record, record_len);
+                status = true;
+            }
+        }
+    }
+
+    return status;
+}
+
+/**
+ * @brief Initializes the object data
+ */
+void bacfile_sramfs_init(void)
+{
+#if defined(BACFILE)
+    bacfile_read_stream_data_callback_set(bacfile_sramfs_read_stream_data);
+    bacfile_read_record_data_callback_set(bacfile_sramfs_read_record_data);
+    bacfile_file_size_callback_set(bacfile_sramfs_file_size);
+    bacfile_file_size_set_callback_set(bacfile_sramfs_file_size_set);
+#endif
+}
--- src/bacnet/basic/sys/bsramfs.h
+++ src/bacnet/basic/sys/bsramfs.h
@@ -1,46 +1,55 @@
 /**
  * @file
- * @brief A dynamic RAM based BACnet File Object implementation.
+ * @brief A static RAM based BACnet File Object implementation.
  * @author Steve Karg
  * @date July 2025
  * @copyright SPDX-License-Identifier: MIT
  */
-#ifndef BACNET_FILE_RAMFS_H
-#define BACNET_FILE_RAMFS_H
+#ifndef BACNET_FILE_SRAMFS_H
+#define BACNET_FILE_SRAMFS_H
 /* BACnet Stack defines - first */
 #include "bacnet/bacdef.h"
+
+struct bacnet_file_sramfs_data {
+    size_t size; /* size of the file in bytes */
+    char *data; /* data buffer */
+    char *pathname; /* pathname of the file */
+    struct bacnet_file_sramfs_data *next; /* pointer to the next file data */
+};
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
 BACNET_STACK_EXPORT
-const char *bacfile_ramfs_file_data(const char *pathname);
+const char *bacfile_sramfs_file_data(const char *pathname);
 BACNET_STACK_EXPORT
-size_t bacfile_ramfs_file_size(const char *pathname);
+bool bacfile_sramfs_file_data_set(const char *pathname, const char *file_data);
 BACNET_STACK_EXPORT
-bool bacfile_ramfs_file_size_set(const char *pathname, size_t file_size);
+size_t bacfile_sramfs_file_size(const char *pathname);
 BACNET_STACK_EXPORT
-size_t bacfile_ramfs_read_stream_data(
+bool bacfile_sramfs_file_size_set(const char *pathname, size_t file_size);
+BACNET_STACK_EXPORT
+size_t bacfile_sramfs_read_stream_data(
     const char *pathname,
     int32_t fileStartPosition,
     uint8_t *fileData,
     size_t fileDataLen);
 BACNET_STACK_EXPORT
-size_t bacfile_ramfs_write_stream_data(
+size_t bacfile_sramfs_write_stream_data(
     const char *pathname,
     int32_t fileStartPosition,
     const uint8_t *fileData,
     size_t fileDataLen);
 BACNET_STACK_EXPORT
-bool bacfile_ramfs_write_record_data(
+bool bacfile_sramfs_write_record_data(
     const char *pathname,
     int32_t fileStartRecord,
     size_t fileIndexRecord,
     const uint8_t *fileData,
     size_t fileDataLen);
 BACNET_STACK_EXPORT
-bool bacfile_ramfs_read_record_data(
+bool bacfile_sramfs_read_record_data(
     const char *pathname,
     int32_t fileStartRecord,
     size_t fileIndexRecord,
@@ -48,9 +57,9 @@ bool bacfile_ramfs_read_record_data(
     size_t fileDataLen);
 
 BACNET_STACK_EXPORT
-void bacfile_ramfs_deinit(void);
+bool bacfile_sramfs_add(struct bacnet_file_sramfs_data *file_data);
 BACNET_STACK_EXPORT
-void bacfile_ramfs_init(void);
+void bacfile_sramfs_init(void);
 
 #ifdef __cplusplus
 }
