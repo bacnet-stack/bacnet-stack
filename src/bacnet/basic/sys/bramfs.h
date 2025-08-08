3f8b8b5619895f86357811c570abc6e1e8068c86
Steve Karg <skarg@users.sourceforge.net>
2025-08-08 15:35:13 -0500

    Added dynamic and static RAM file systems to use with file objects. (#1058)
    * Added dynamic RAM file system to use with basic bacnet file object.
    
    * Added static RAM file system to use with basic bacnet file object.
    
    * Added check for read-only during AtomicWriteFile service API for BACnet File object.
    
    * Change stm32f4xx example to use static RAM file system.
    
    * Fixed bacfile_count() function return type

---
+++ src/bacnet/basic/sys/bramfs.h
@@ -0,0 +1,58 @@
+/**
+ * @file
+ * @brief A dynamic RAM based BACnet File Object implementation.
+ * @author Steve Karg
+ * @date July 2025
+ * @copyright SPDX-License-Identifier: MIT
+ */
+#ifndef BACNET_FILE_RAMFS_H
+#define BACNET_FILE_RAMFS_H
+/* BACnet Stack defines - first */
+#include "bacnet/bacdef.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+BACNET_STACK_EXPORT
+const char *bacfile_ramfs_file_data(const char *pathname);
+BACNET_STACK_EXPORT
+size_t bacfile_ramfs_file_size(const char *pathname);
+BACNET_STACK_EXPORT
+bool bacfile_ramfs_file_size_set(const char *pathname, size_t file_size);
+BACNET_STACK_EXPORT
+size_t bacfile_ramfs_read_stream_data(
+    const char *pathname,
+    int32_t fileStartPosition,
+    uint8_t *fileData,
+    size_t fileDataLen);
+BACNET_STACK_EXPORT
+size_t bacfile_ramfs_write_stream_data(
+    const char *pathname,
+    int32_t fileStartPosition,
+    const uint8_t *fileData,
+    size_t fileDataLen);
+BACNET_STACK_EXPORT
+bool bacfile_ramfs_write_record_data(
+    const char *pathname,
+    int32_t fileStartRecord,
+    size_t fileIndexRecord,
+    const uint8_t *fileData,
+    size_t fileDataLen);
+BACNET_STACK_EXPORT
+bool bacfile_ramfs_read_record_data(
+    const char *pathname,
+    int32_t fileStartRecord,
+    size_t fileIndexRecord,
+    uint8_t *fileData,
+    size_t fileDataLen);
+
+BACNET_STACK_EXPORT
+void bacfile_ramfs_deinit(void);
+BACNET_STACK_EXPORT
+void bacfile_ramfs_init(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+#endif
