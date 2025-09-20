9dbe3512e0a9e705e9f26940ba3f667dc170978c
Ed <edward@connect-ex.com>
2025-09-20 17:20:01 +0100

    More clang format fixups

--- src/bacnet/basic/object/ao.c
+++ src/bacnet/basic/object/ao.c
@@ -624,7 +624,8 @@ BACNET_ENGINEERING_UNITS Analog_Output_Units(uint32_t object_instance)
  *
  * @return true if the units property value was set
  */
-bool Analog_Output_Units_Set(uint32_t object_instance, BACNET_ENGINEERING_UNITS units)
+bool Analog_Output_Units_Set(
+    uint32_t object_instance, BACNET_ENGINEERING_UNITS units)
 {
     bool status = false;
     struct object_data *pObject;
