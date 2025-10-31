7144cfe12e43fa341f3ad093d28882daa4114116
Steve Karg <skarg@users.sourceforge.net>
2025-10-30 20:48:45 -0500

    Updated the bvlc6.sh to enable registration with foreign device

--- bin/bvlc6.sh
+++ bin/bvlc6.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-echo Example of parameters for Foreign Device Registration
-BACNET_IP_PORT=47809
-export BACNET_IP_PORT
-echo This console will use port ${BACNET_IP_PORT} to communicate.
+echo Example of parameters for Foreign Device Registration for IPv6
+BACNET_BIP6_PORT=47809
+export BACNET_BIP6_PORT
+echo This console will use port ${BACNET_BIP6_PORT} to communicate.
 echo
-BACNET_BBMD_PORT=47808
-export BACNET_BBMD_PORT
-echo The BBMD is located at the port ${BACNET_BBMD_PORT} and
+BACNET_BBMD6_PORT=47808
+export BACNET_BBMD6_PORT
+echo The BBMD is located at the port ${BACNET_BBMD6_PORT} and
 echo is at the dotted IP address passed on the command line.
-BACNET_BBMD_ADDRESS=${1}
-export BACNET_BBMD_ADDRESS
-echo The BBMD IP address is ${BACNET_BBMD_ADDRESS}
+BACNET_BBMD6_ADDRESS=${1}
+export BACNET_BBMD6_ADDRESS
+echo The BBMD IP address is ${BACNET_BBMD6_ADDRESS}
 echo When the demo client applications see the BBMD address,
 echo they register as a Foreign Device to it.
 echo
