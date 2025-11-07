dd96635727b245c8932a5d00a3a00f89d809766b
Steve Karg <skarg@users.sourceforge.net>
2025-11-07 16:13:03 -0600

    Revised the BACnet/SC server script to only enable the HUB function

--- bin/bsc-server.sh
+++ bin/bsc-server.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 echo "Example of parameters for BACnet/SC server"
 
-BACNET_SC_PRIMARY_HUB_URI="wss://127.0.0.1:50050"
-export BACNET_SC_PRIMARY_HUB_URI
-BACNET_SC_FAILOVER_HUB_URI="wss://127.0.0.1:50050"
-export BACNET_SC_FAILOVER_HUB_URI
-
 BACNET_SC_ISSUER_1_CERTIFICATE_FILE="certs/ca_cert.pem"
 export BACNET_SC_ISSUER_1_CERTIFICATE_FILE
 
@@ -24,6 +19,12 @@ export BACNET_SC_OPERATIONAL_CERTIFICATE_PRIVATE_KEY_FILE
 # port number, like "eth0:50050"
 BACNET_SC_HUB_FUNCTION_BINDING="50050"
 export BACNET_SC_HUB_FUNCTION_BINDING
+
+# Need if the device is not a HUB
+#BACNET_SC_PRIMARY_HUB_URI="wss://127.0.0.1:50050"
+#export BACNET_SC_PRIMARY_HUB_URI
+#BACNET_SC_FAILOVER_HUB_URI="wss://127.0.0.1:50050"
+#export BACNET_SC_FAILOVER_HUB_URI
 
 # Need if the device allows the direct connect.
 # Possible values: or port number, like "5000" or pair interface name and
