9a335b545eddb118d4b630dee2e68aee561b877e
Ryan Mulder <ryan.mulder@antrum.com>
2025-12-03 17:05:26 -0500

    Added `_Address` versions of a few services, primarily for interacting with MS/TP slaves (#1174)
    * add address versions of Send_Write_Property_Request and Send_Read_Property_Multiple_Request
    
    * add Send_COV_Subscribe_Address

--- src/bacnet/basic/service/s_wp.h
+++ src/bacnet/basic/service/s_wp.h
@@ -26,12 +26,33 @@ extern "C" {
 #endif /* __cplusplus */
 
 BACNET_STACK_EXPORT
+uint8_t Send_Write_Property_Request_Address(
+    BACNET_ADDRESS *dest,
+    uint16_t max_apdu,
+    BACNET_OBJECT_TYPE object_type,
+    uint32_t object_instance,
+    BACNET_PROPERTY_ID object_property,
+    const BACNET_APPLICATION_DATA_VALUE *object_value,
+    uint8_t priority,
+    uint32_t array_index);
+BACNET_STACK_EXPORT
 uint8_t Send_Write_Property_Request(
     uint32_t device_id, /* destination device */
     BACNET_OBJECT_TYPE object_type,
     uint32_t object_instance,
     BACNET_PROPERTY_ID object_property,
     const BACNET_APPLICATION_DATA_VALUE *object_value,
+    uint8_t priority,
+    uint32_t array_index);
+BACNET_STACK_EXPORT
+uint8_t Send_Write_Property_Request_Data_Address(
+    BACNET_ADDRESS *dest,
+    uint16_t max_apdu,
+    BACNET_OBJECT_TYPE object_type,
+    uint32_t object_instance,
+    BACNET_PROPERTY_ID object_property,
+    const uint8_t *application_data,
+    int application_data_len,
     uint8_t priority,
     uint32_t array_index);
 BACNET_STACK_EXPORT
