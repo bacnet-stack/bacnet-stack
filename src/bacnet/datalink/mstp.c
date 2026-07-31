03c6b7bed68f3c4e6f69bf9e1c29e1a5a4bfea11
Steve Karg <skarg@users.sourceforge.net>
2026-07-31 07:50:08 -0500

    fix: simplify cobs_frame_decode call parameters in MSTP frame processing

--- src/bacnet/datalink/mstp.c
+++ src/bacnet/datalink/mstp.c
@@ -565,9 +565,8 @@ void MSTP_Receive_Frame_FSM(struct mstp_port_struct_t *mstp_port)
                         /* decode in-place: FrameTooLong check ensures
                            decoded output fits in InputBuffer */
                         mstp_port->DataLength = cobs_frame_decode(
-                            &mstp_port->InputBuffer[0],
-                            mstp_port->InputBufferSize, mstp_port->InputBuffer,
-                            mstp_port->Index);
+                            mstp_port->InputBuffer, mstp_port->InputBufferSize,
+                            mstp_port->InputBuffer, mstp_port->Index);
                         if (mstp_port->DataLength > 0) {
                             /* GoodCRC */
                             if (mstp_port->receive_state ==
