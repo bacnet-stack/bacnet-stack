7ee148bdc003747884bbd13fef9c067ee90e77a5
Steve Karg <skarg@users.sourceforge.net>
2026-09-07 07:34:37 -0500

    Sync the default size of the device address size cache in config.h file

--- src/bacnet/config.h
+++ src/bacnet/config.h
@@ -187,10 +187,8 @@
 /* The address cache is used for binding to BACnet devices. */
 /* Entries are stored in a Keylist keyed by device instance. */
 /* This value is the maximum number of cache entries. */
-/* If your device is a simple server and does not need to bind, */
-/* then you don't need to use this. */
 #if !defined(MAX_ADDRESS_CACHE)
-#define MAX_ADDRESS_CACHE 255
+#define MAX_ADDRESS_CACHE 4096
 #endif
 
 /* some modules have debugging enabled using PRINT_ENABLED */
